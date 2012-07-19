001d29664cb1dd9bec2ad3518b0a02e04108a238
Evandro Flores <eof@eof.com.br>
2012-07-19 14:27:29 -0300

    login page

--- abyss/urls.py
+++ abyss/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'abyss.views.login', name='login'),
+    url(r'^$', 'auth.views.login', name='login'),
+    url(r'^login$', 'auth.views.login', name='login'),
 )
