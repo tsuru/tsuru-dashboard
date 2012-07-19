6caeb619c1995933d9f4e2b8148e8f069ef238d4
Glaucia Peres <gauperes@gmail.com>
2012-07-19 16:06:38 -0300

    adding login form to view context.

--- auth/views.py
+++ auth/views.py
@@ -1,4 +1,6 @@
 from django.template.response import TemplateResponse
 
+from auth.forms import LoginForm
+
 def login(request):
-    return TemplateResponse(request, 'auth/login.html')
+    return TemplateResponse(request, 'auth/login.html', context={'login_form': LoginForm()})
