62f57891bedea061b66fe957ae106fe59a6e4174
Siminino <victor.areas@corp.globo.com>
2012-07-19 16:16:39 -0300

    create team view

--- auth/tests/test_views.py
+++ auth/tests/test_views.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.test.client import RequestFactory
+from django.http import Http404
 
-from auth.views import login
+from auth.views import login, team
+from auth.forms import TeamForm
 
 
 class LoginViewTest(TestCase):
@@ -9,3 +11,25 @@ class LoginViewTest(TestCase):
         request = RequestFactory().get('/')
         response = login(request)
         self.assertEqual('auth/login.html', response.template_name)
+
+
+class TeamViewTest(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get('/')
+        self.response = team(self.request)
+
+    def test_team_should_render_expected_template(self):
+        self.assertEqual('auth/team.html', self.response.template_name)
+
+    def test_context_should_contain_form(self):
+        self.assertIn('form', self.response.context_data.keys())
+
+    def test_form_in_context_should_has_a_instance_of_TeamForm(self):
+        form = self.response.context_data.get('form')
+        self.assertTrue(isinstance(form, TeamForm))
+
+    def test_get_request_team_url_should_return_200(self):
+        try:
+            self.client.get('/team/')
+        except Http404:
+            assert False
--- auth/views.py
+++ auth/views.py
@@ -1,4 +1,11 @@
 from django.template.response import TemplateResponse
 
+from auth.forms import TeamForm
+
 def login(request):
     return TemplateResponse(request, 'auth/login.html')
+
+def team(request):
+    context = {}
+    context['form'] = TeamForm()
+    return TemplateResponse(request, 'auth/team.html', context)
