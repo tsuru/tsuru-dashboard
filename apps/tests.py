189292b27f4d3bc9d38f6be53f0d2c22d05e2f02
Andrews Medina <andrewsmedina@gmail.com>
2012-07-20 11:59:36 -0300

    returns error msg when post returns error on create app

--- apps/tests.py
+++ apps/tests.py
@@ -1,3 +1,5 @@
+from mock import patch, Mock
+
 from django.test import TestCase
 from django.test.client import RequestFactory
 
@@ -26,3 +28,22 @@ class CreateAppViewTest(TestCase):
         response = CreateApp().get(request)
         app_form = response.context_data['app_form']
         self.assertIsInstance(app_form, forms.AppForm)
+
+    def test_post_with_invalid_name_should_return_500(self):
+        request = RequestFactory().post("/", {"name": "myepe"})
+        request.session = {}
+        response_mock = Mock()
+        with patch('requests.post') as post:
+            response_mock.status_code = 500
+            response_mock.content = 'Error'
+            post.side_effect = Mock(return_value=response_mock)
+            response = CreateApp().post(request)
+            self.assertEqual('Error', response.context_data.get('msg'))
+
+    def test_post_without_name_should_return_form_with_errors(self):
+        request = RequestFactory().post("/", {"name": ""})
+        request.session = {}
+        response = CreateApp().post(request)
+        form =  response.context_data.get('form')
+        self.assertIn('name', form.errors)
+        self.assertIn(u'This field is required.', form.errors.get('name'))
--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,9 @@
+import json
+import requests
+
 from django.views.generic.base import View
 from django.template.response import TemplateResponse
+from django.conf import settings
 
 from apps.forms import AppForm
 
@@ -8,3 +12,14 @@ class CreateApp(View):
 
     def get(self, request):
         return TemplateResponse(request, "apps/create.html", {"app_form": AppForm()})
+
+    def post(self, request):
+        form = AppForm(request.POST)
+        if form.is_valid():
+            authorization = {'authorization': request.session.get('tsuru_token')}
+            response = requests.post('%s/teams' % settings.TSURU_HOST,
+                                     data=json.dumps(form.data),
+                                     headers=authorization)
+            if not response.status_code == 200:
+                return TemplateResponse(request, 'auth/team.html', {'form': form, 'msg': response.content})
+        return TemplateResponse(request, 'apps/create.html', {'form': form})
