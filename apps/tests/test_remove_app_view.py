a3941d4617edff062ff8f03cc14f9d49c1ef6148
Andrews Medina <andrewsmedina@gmail.com>
2013-04-12 18:45:18 -0300

    refactored app remove tests.

--- apps/tests/test_remove_app_view.py
+++ apps/tests/test_remove_app_view.py
@@ -6,22 +6,23 @@ from apps.views import RemoveApp
 
 import mock
 
+
 class RemoveAppTestCase(TestCase):
-    def test_should_returns_404_when_app_does_not_exists(self):
-        request = RequestFactory().get("/name/remove")
-        request.session = {"tsuru_token":"admin"}
-        with mock.patch('requests.delete') as delete:
-            delete.return_value = mock.Mock(status_code=404, text="app not found")
-            response = RemoveApp.as_view()(request, name="appname")
+    def setUp(self):
+        self.request = RequestFactory().get("/name/remove")
+        self.request.session = {"tsuru_token":"admin"}
+
+    @mock.patch('requests.delete')
+    def test_should_returns_404_when_app_does_not_exists(self, delete):
+        delete.return_value = mock.Mock(status_code=404, text="app not found")
+        response = RemoveApp.as_view()(self.request, name="appname")
         self.assertEqual(404, response.status_code)
         self.assertEqual("app not found", response.content)
 
-    def test_should_redirect_to_the_app_list(self):
-        request = RequestFactory().get("/name/remove")
-        request.session = {"tsuru_token":"admin"}
-        with mock.patch('requests.delete') as delete:
-            delete.return_value = mock.Mock(status_code=200)
-            response = RemoveApp.as_view()(request, name="appname")
+    @mock.patch('requests.delete')
+    def test_should_redirect_to_the_app_list(self, delete):
+        delete.return_value = mock.Mock(status_code=200)
+        response = RemoveApp.as_view()(self.request, name="appname")
         self.assertEqual(302, response.status_code)
         self.assertIsInstance(response, HttpResponseRedirect)
         self.assertEqual("/app", response['Location'])
