28db1cb28e1027553c53e44682aa4332e2adf3e8
Andrews Medina <andrewsmedina@gmail.com>
2013-05-02 13:01:11 -0300

    applied pep8 rules for auth/tests/test_key_view.py.

--- auth/tests/test_key_view.py
+++ auth/tests/test_key_view.py
@@ -38,23 +38,26 @@ class KeyViewTest(TestCase):
         except Http404:
             assert False
 
-    def test_post_with_name_should_send_request_post_to_tsuru_with_args_expected(self):
+    def test_post_with_name_should_ssend_request_post_to_tsuru(self):
         self.request_post.session = {'tsuru_token': 'tokentest'}
         with patch('requests.post') as post:
             Key().post(self.request_post)
             self.assertEqual(1, post.call_count)
-            post.assert_called_with('%s/users/keys' % settings.TSURU_HOST,
-                                    data='{"key": "test-key-qq"}',
-                                    headers={'authorization': self.request_post.session['tsuru_token']})
+            post.assert_called_with(
+                '%s/users/keys' % settings.TSURU_HOST,
+                data='{"key": "test-key-qq"}',
+                headers={'authorization':
+                         self.request_post.session['tsuru_token']})
 
-    def test_post_with_valid_key_should_return_context_with_message_expected(self):
+    def test_valid_postshould_return_message_expected(self):
         with patch('requests.post') as post:
             self.response_mock.status_code = 200
             post.side_effect = Mock(return_value=self.response_mock)
             response = Key().post(self.request_post)
-            self.assertEqual("The Key was successfully added", response.context_data.get('message'))
+            self.assertEqual("The Key was successfully added",
+                             response.context_data.get('message'))
 
-    def test_post_with_invalid_key_should_return_error_message_expected_on_context(self):
+    def test_invalid_post_should_return_error_message(self):
         with patch('requests.post') as post:
             self.response_mock.status_code = 500
             self.response_mock.content = 'Error'
@@ -68,7 +71,8 @@ class KeyViewTest(TestCase):
             post.side_effect = Mock(return_value=self.response_mock)
             response = Key().post(self.request_post)
             self.assertIn('form', response.context_data.keys())
-            self.assertTrue(isinstance(response.context_data.get('form'), KeyForm))
+            self.assertTrue(isinstance(response.context_data.get('form'),
+                                       KeyForm))
 
     def test_post_with_invalid_key_should_return_context_with_form(self):
         with patch('requests.post') as post:
@@ -77,7 +81,8 @@ class KeyViewTest(TestCase):
             post.side_effect = Mock(return_value=self.response_mock)
             response = Key().post(self.request_post)
             self.assertIn('form', response.context_data.keys())
-            self.assertTrue(isinstance(response.context_data.get('form'), KeyForm))
+            self.assertTrue(isinstance(response.context_data.get('form'),
+                                       KeyForm))
 
     def test_post_without_key_should_not_send_request_to_tsuru(self):
         with patch('requests.post') as post:
@@ -85,7 +90,6 @@ class KeyViewTest(TestCase):
             request.session = {}
             Key().post(request)
             self.assertEqual(0, post.call_count)
-
 
     def test_post_without_key_should_return_form_with_errors(self):
         with patch('requests.post'):
@@ -95,4 +99,5 @@ class KeyViewTest(TestCase):
             self.assertIn('form', response.context_data.keys())
             form = response.context_data.get('form')
             self.assertTrue(isinstance(form, KeyForm))
-            self.assertEqual(u'This field is required.', form.errors.get('key')[0])
+            self.assertEqual(u'This field is required.',
+                             form.errors.get('key')[0])
