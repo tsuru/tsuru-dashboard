458004709c9017c659782673bb7f1c638c45b4be
Andrews Medina <andrewsmedina@gmail.com>
2013-05-02 00:00:30 -0300

    pep8 rules in urls.py.

--- abyss/urls.py
+++ abyss/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from auth.views import Login, Logout, Signup, Team, Key
-from apps.views import CreateApp, AppAddTeam, Run, ListApp, RemoveApp, AppLog, AppDetail, AppEnv, AppTeams
+from apps.views import (CreateApp, AppAddTeam, Run, ListApp, RemoveApp, AppLog,
+                        AppDetail, AppEnv, AppTeams)
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', Login.as_view(), name='login'),
-
     url(r'^login$', Login.as_view(), name='login'),
     url(r'^logout$', Logout.as_view(), name='logout'),
     url(r'^team/$', Team.as_view(), name='team'),
@@ -17,12 +18,16 @@ urlpatterns = patterns('',
     url(r'^apps/$', ListApp.as_view(), name='list-app'),
     url(r'^apps/create/$', CreateApp.as_view(), name='create-app'),
     url(r'^app/run/$', Run.as_view(), name='run'),
-    url(r'^app/(?P<app_name>[\w-]+)/$', AppDetail.as_view(), name='detail-app'),
-    url(r'^app/(?P<name>[\w-]+)/remove/$', RemoveApp.as_view(), name='remove_app'),
+    url(r'^app/(?P<app_name>[\w-]+)/$',
+        AppDetail.as_view(), name='detail-app'),
+    url(r'^app/(?P<name>[\w-]+)/remove/$',
+        RemoveApp.as_view(), name='remove_app'),
     url(r'^app/(?P<app_name>[\w-]+)/log/$', AppLog.as_view(), name='app_log'),
     url(r'^app/(?P<app_name>[\w-]+)/env/$', AppEnv.as_view(), name='get-env'),
-    url(r'^app/(?P<app_name>[\w-]+)/teams/$', AppTeams.as_view(), name='app-teams'),
-    url(r'^app/(?P<app_name>[\w-]+)/team/add/$', AppAddTeam.as_view(), name='app-add-team'),
+    url(r'^app/(?P<app_name>[\w-]+)/teams/$',
+        AppTeams.as_view(), name='app-teams'),
+    url(r'^app/(?P<app_name>[\w-]+)/team/add/$',
+        AppAddTeam.as_view(), name='app-add-team'),
     url(r'^envs/$', "auth.views.env_vars", name='envs'),
 )
 
