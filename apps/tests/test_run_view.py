8e8d09591df0b6e3295c56369b40197d30254d57
Andrews Medina <andrewsmedina@gmail.com>
2013-05-02 00:47:58 -0300

    applied pep8 for apps/tests/test_run_view.py.

--- apps/tests/test_run_view.py
+++ apps/tests/test_run_view.py
@@ -16,7 +16,9 @@ class RunViewTest(TestCase):
         self.factory = RequestFactory()
         self.request = self.factory.get('/')
         self.response = Run().get(self.request)
-        self.request_post = self.factory.post('/', {'app': 'app-test', 'command': 'command-test'})
+        self.request_post = self.factory.post(
+            '/',
+            {'app': 'app-test', 'command': 'command-test'})
         self.request_post.session = {}
         self.response_mock = Mock()
 
@@ -39,24 +41,30 @@ class RunViewTest(TestCase):
         except Http404:
             assert False
 
-    def test_post_with_app_and_command_should_send_request_post_to_tsuru_with_args_expected(self):
+    def test_post_with_app_and_command_sends_post(self):
         self.request_post.session = {'tsuru_token': 'tokentest'}
         with patch('requests.post') as post:
             Run().post(self.request_post)
             self.assertEqual(1, post.call_count)
-            post.assert_called_with('%s/apps/app-test/run' % settings.TSURU_HOST,
-                                    data=u'command-test',
-                                    headers={'authorization': self.request_post.session['tsuru_token']})
+            post.assert_called_with(
+                '%s/apps/app-test/run' % settings.TSURU_HOST,
+                data=u'command-test',
+                headers={'authorization':
+                         self.request_post.session['tsuru_token']})
 
-    def test_post_with_valid_app_and_command_should_return_context_with_message_expected(self):
+    def test_post_with_valid_app_and_command(self):
         with patch('requests.post') as post:
             self.response_mock.status_code = 200
             self.response_mock.content = "command runned"
             post.side_effect = Mock(return_value=self.response_mock)
             response = Run().post(self.request_post)
-            self.assertEqual("command runned", response.context_data.get('message'))
+            self.assertEqual("command runned",
+                             response.context_data.get('message'))
+            self.assertIn('form', response.context_data.keys())
+            self.assertTrue(isinstance(response.context_data.get('form'),
+                                       RunForm))
 
-    def test_post_with_invalid_app_or_command_should_return_error_message_expected_on_context(self):
+    def test_post_with_invalid_app_or_command_error_in_context(self):
         with patch('requests.post') as post:
             self.response_mock.status_code = 500
             self.response_mock.content = 'Error'
@@ -64,36 +72,32 @@ class RunViewTest(TestCase):
             response = Run().post(self.request_post)
             self.assertEqual('Error', response.context_data.get('errors'))
 
-    def test_post_with_valid_app_and_command_should_return_context_with_form(self):
-        with patch('requests.post') as post:
-            self.response_mock.status_code = 200
-            post.side_effect = Mock(return_value=self.response_mock)
-            response = Run().post(self.request_post)
-            self.assertIn('form', response.context_data.keys())
-            self.assertTrue(isinstance(response.context_data.get('form'), RunForm))
-
-    def test_post_with_invalid_app_or_command_should_return_context_with_form(self):
+    def test_post_with_invalid_app_or_command(self):
         with patch('requests.post') as post:
             self.response_mock.status_code = 500
             self.response_mock.content = 'Error'
             post.side_effect = Mock(return_value=self.response_mock)
             response = Run().post(self.request_post)
             self.assertIn('form', response.context_data.keys())
-            self.assertTrue(isinstance(response.context_data.get('form'), RunForm))
+            self.assertTrue(isinstance(response.context_data.get('form'),
+                                       RunForm))
 
     def test_post_without_app_should_not_send_request_to_tsuru(self):
         with patch('requests.post') as post:
-            request = self.factory.post('/', {'app': '', 'command': 'comand-test'})
+            request = self.factory.post('/', {'app': '',
+                                              'command': 'comand-test'})
             request.session = {}
             Run().post(request)
             self.assertEqual(0, post.call_count)
 
     def test_post_without_app_should_return_form_with_errors(self):
         with patch('requests.post'):
-            request = self.factory.post('/', {'app': '', 'command': 'command-test'})
+            request = self.factory.post('/', {'app': '',
+                                              'command': 'command-test'})
             request.session = {}
             response = Run().post(request)
             self.assertIn('form', response.context_data.keys())
             form = response.context_data.get('form')
             self.assertTrue(isinstance(form, RunForm))
-            self.assertEqual(u'This field is required.', form.errors.get('app')[0])
+            self.assertEqual(u'This field is required.',
+                             form.errors.get('app')[0])
