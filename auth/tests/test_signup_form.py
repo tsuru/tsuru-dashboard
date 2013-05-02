c3591ef9ea3083a25d0c431a79e4963fa2535b68
Andrews Medina <andrewsmedina@gmail.com>
2013-05-02 01:07:35 -0300

    applied pep8 for auth/tests/test_signup_form.py.

--- auth/tests/test_signup_form.py
+++ auth/tests/test_signup_form.py
@@ -4,18 +4,21 @@ from auth import forms
 
 
 class SignupFormTest(TestCase):
-    def test_signup_form_should_have_email_password_and_same_password_again_fields(self):
+    def test_fields(self):
         self.assertIn('email', forms.SignupForm.base_fields)
         self.assertIn('password', forms.SignupForm.base_fields)
         self.assertIn('same_password_again', forms.SignupForm.base_fields)
 
-    def test_password_and_same_password_again_should_have_same_text(self):
-        data = {'email':'test@test.com', 'password':'abc123', 'same_password_again': 'abc123'}
+    def test_password_confirmation_is_valid(self):
+        data = {'email': 'test@test.com', 'password': 'abc123',
+                'same_password_again': 'abc123'}
         form = forms.SignupForm(data)
         self.assertTrue(form.is_valid())
 
-    def test_shuld_show_an_error_if_password_and_same_password_again_should_are_not_the_same(self):
-        data = {'email':'test@test.com', 'password':'abc123', 'same_password_again': 'not the same'}
+    def test_password_confirmation_is_invalid(self):
+        data = {'email': 'test@test.com', 'password': 'abc123',
+                'same_password_again': 'not the same'}
         form = forms.SignupForm(data)
         self.assertFalse(form.is_valid())
-        self.assertIn(u'You must type the same password twice!', form.errors['same_password_again'])
+        self.assertIn(u'You must type the same password twice!',
+                      form.errors['same_password_again'])
