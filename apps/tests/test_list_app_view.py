adeb7ae62f15d5967d2caad06877c118c09df729
Andrews Medina <andrewsmedina@gmail.com>
2013-05-02 01:00:46 -0300

    applied pep8 in apps/tests/test_list_app_view.py.

--- apps/tests/test_list_app_view.py
+++ apps/tests/test_list_app_view.py
@@ -9,7 +9,7 @@ from apps.views import ListApp
 class ListAppViewTest(TestCase):
     def test_should_use_list_template(self):
         request = RequestFactory().get("/")
-        request.session = {"tsuru_token":"admin"}
+        request.session = {"tsuru_token": "admin"}
         response_mock = Mock()
         with patch('requests.get') as get:
             response_mock.status_code = 204
@@ -17,14 +17,16 @@ class ListAppViewTest(TestCase):
             response = ListApp().get(request)
             self.assertEqual("apps/list.html", response.template_name)
 
-    def teste_should_get_list_of_apps_from_tsuru(self):
+    @patch('requests.get')
+    def teste_should_get_list_of_apps_from_tsuru(self, get):
         request = RequestFactory().get("/")
-        request.session = {"tsuru_token":"admin"}
-        with patch('requests.get') as get:
-            expected = [{"framework":"python","name":"pacote",
-                         "repository":"git@tsuru.com:pacote.git","state":"creating"}]
-            get.return_value = Mock(status_code=200, json=expected)
-            response = ListApp().get(request)
-            self.assertEqual([{"framework":"python","name":"pacote",
-                               "repository":"git@tsuru.com:pacote.git","state":"creating"}],
-                             response.context_data.get("apps"))
+        request.session = {"tsuru_token": "admin"}
+        expected = [{"framework": "python", "name": "pacote",
+                     "repository": "git@tsuru.com:pacote.git",
+                     "state": "creating"}]
+        get.return_value = Mock(status_code=200, json=expected)
+        response = ListApp().get(request)
+        self.assertEqual([{"framework": "python", "name": "pacote",
+                           "repository": "git@tsuru.com:pacote.git",
+                           "state": "creating"}],
+                         response.context_data.get("apps"))
