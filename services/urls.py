64af594a26c60f8da8215d5f4988bb2bea416f7a
Andrews Medina <andrewsmedina@gmail.com>
2013-05-04 15:40:10 -0300

    added url for services info.
    related to #8.

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 
-from services.views import ListService
+from services.views import ListService, ServiceDetail
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', ListService.as_view(), name='service-list'),
+    url(r'^(?P<service_name>[\w-]+)/$', ServiceDetail.as_view(),
+        name='service-detail'),
 )
