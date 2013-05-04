03aab4899f5f02d749b3ce93c19d7ac4f8ff3f72
Andrews Medina <andrewsmedina@gmail.com>
2013-05-04 09:37:03 -0300

    refactored mock for test_app_team_view.py.
    using decorator instead context manager.

--- apps/tests/test_app_team_view.py
+++ apps/tests/test_app_team_view.py
@@ -13,7 +13,8 @@ import mock
 
 class AppTeamsViewTest(TestCase):
 
-    def setUp(self):
+    @patch('requests.get')
+    def setUp(self, get):
         self.factory = RequestFactory()
         self.request = self.factory.get('/')
         self.request.session = {'tsuru_token': 'tokentest'}
@@ -21,9 +22,8 @@ class AppTeamsViewTest(TestCase):
         self.response_mock = Mock()
         self.response_mock.status_code = 200
         self.response_mock.content = '{}'
-        with patch('requests.get') as get:
-            get.return_value = self.response_mock
-            self.response = AppTeams().get(self.request, self.app_name)
+        get.return_value = self.response_mock
+        self.response = AppTeams().get(self.request, self.app_name)
 
     def test_should_require_login_to_set_env(self):
         assert issubclass(AppTeams, LoginRequiredView)
@@ -34,14 +34,15 @@ class AppTeamsViewTest(TestCase):
     def test_context_should_contain_app(self):
         self.assertIn('app', self.response.context_data.keys())
 
-    def test_get_sends_request_to_tsuru_with_args_expected(self):
-        with patch('requests.get') as get:
-            AppTeams().get(self.request, self.app_name)
-            get.assert_called_with(
-                '%s/apps/%s' % (settings.TSURU_HOST, self.app_name),
-                headers={'authorization': self.request.session['tsuru_token']})
+    @patch('requests.get')
+    def test_get_sends_request_to_tsuru_with_args_expected(self, get):
+        AppTeams().get(self.request, self.app_name)
+        get.assert_called_with(
+            '%s/apps/%s' % (settings.TSURU_HOST, self.app_name),
+            headers={'authorization': self.request.session['tsuru_token']})
 
-    def test_get_with_valid_app_should_return_expected_context(self):
+    @mock.patch("requests.get")
+    def test_get_with_valid_app_should_return_expected_context(self, get):
         expected = {
             "Name": "app1",
             "Framework": "php",
@@ -53,22 +54,19 @@ class AppTeamsViewTest(TestCase):
             ],
             "Teams": ["tsuruteam", "crane"]
         }
-        with mock.patch("requests.get") as get:
-            get.return_value = mock.Mock(status_code=200, json=expected)
-            response = AppTeams.as_view()(self.request, app_name="app1")
+        get.return_value = mock.Mock(status_code=200, json=expected)
+        response = AppTeams.as_view()(self.request, app_name="app1")
         self.assertDictEqual(expected, response.context_data["app"])
 
-    def test_get_with_invalid_app_should_return_context_with_error(self):
+    @patch('requests.get')
+    def test_get_with_invalid_app_should_return_context_with_error(self, get):
         self.response_mock.status_code = 404
         self.response_mock.content = 'App not found'
-
-        with patch('requests.get') as get:
-            get.return_value = self.response_mock
-            response = AppTeams().get(self.request, 'invalid-app')
-
-            self.assertIn('errors', response.context_data.keys())
-            self.assertEqual(self.response_mock.content,
-                             response.context_data['errors'])
+        get.return_value = self.response_mock
+        response = AppTeams().get(self.request, 'invalid-app')
+        self.assertIn('errors', response.context_data.keys())
+        self.assertEqual(self.response_mock.content,
+                         response.context_data['errors'])
 
     def test_get_request_run_url_should_return_200(self):
         try:
