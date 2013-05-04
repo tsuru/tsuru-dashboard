d7b0808e8d78b7c00bd997151ad1b1bb612ab53c
Andrews Medina <andrewsmedina@gmail.com>
2013-05-04 09:42:51 -0300

    refactored mocks for test_list_app_view.py.
    using decorator instead context manager.

--- apps/tests/test_list_app_view.py
+++ apps/tests/test_list_app_view.py
@@ -7,15 +7,15 @@ from apps.views import ListApp
 
 
 class ListAppViewTest(TestCase):
-    def test_should_use_list_template(self):
+    @patch('requests.get')
+    def test_should_use_list_template(self, get):
         request = RequestFactory().get("/")
         request.session = {"tsuru_token": "admin"}
         response_mock = Mock()
-        with patch('requests.get') as get:
-            response_mock.status_code = 204
-            get.side_effect = Mock(return_value=response_mock)
-            response = ListApp().get(request)
-            self.assertEqual("apps/list.html", response.template_name)
+        response_mock.status_code = 204
+        get.side_effect = Mock(return_value=response_mock)
+        response = ListApp().get(request)
+        self.assertEqual("apps/list.html", response.template_name)
 
     @patch('requests.get')
     def teste_should_get_list_of_apps_from_tsuru(self, get):
