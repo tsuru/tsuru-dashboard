37fbf7f1350b07e21b65e8196771b2c7c25ce612
Andrews Medina <andrewsmedina@gmail.com>
2013-05-04 09:56:59 -0300

    refactored mocks for test_signup_view.py.
    using decorator instead context manager.

--- auth/tests/test_signup_view.py
+++ auth/tests/test_signup_view.py
@@ -26,19 +26,18 @@ class SignupViewTest(TestCase):
         request = self.factory.post('/signup', data)
         response = Signup().post(request)
         form = response.context_data['signup_form']
-
         self.assertIn(u'This field is required.', form.errors['email'])
         self.assertIn(u'This field is required.', form.errors['password'])
         self.assertIn(u'This field is required.',
                       form.errors['same_password_again'])
 
-    def test_post_sends_to_tsuru_with_args_expected(self):
+    @patch('requests.post')
+    def test_post_sends_to_tsuru_with_args_expected(self, post):
         data = {'email': 'test@test.com', 'password': 'abc123',
                 'same_password_again': 'abc123'}
         request = self.factory.post('/signup', data)
-        with patch('requests.post') as post:
-            Signup().post(request)
-            self.assertEqual(1, post.call_count)
-            post.assert_called_with(
-                '%s/users' % settings.TSURU_HOST,
-                data='{"password": "abc123", "email": "test@test.com"}')
+        Signup().post(request)
+        self.assertEqual(1, post.call_count)
+        post.assert_called_with(
+            '%s/users' % settings.TSURU_HOST,
+            data='{"password": "abc123", "email": "test@test.com"}')
