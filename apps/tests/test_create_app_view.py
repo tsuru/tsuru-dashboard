b52d71766e95840762cfe5ca6982b79b2610ae36
Andrews Medina <andrewsmedina@gmail.com>
2013-05-04 09:40:16 -0300

    refactored mock for test_create_app_view.py.
    using decorator instead context manager.

--- apps/tests/test_create_app_view.py
+++ apps/tests/test_create_app_view.py
@@ -21,18 +21,18 @@ class CreateAppViewTest(TestCase):
         app_form = response.context_data['app_form']
         self.assertIsInstance(app_form, forms.AppForm)
 
-    def test_post_with_invalid_name_should_return_500(self):
+    @patch('requests.post')
+    def test_post_with_invalid_name_should_return_500(self, post):
         request = RequestFactory().post(
             "/",
             {"name": "myepe", "framework": "python"})
         request.session = {}
         response_mock = Mock()
-        with patch('requests.post') as post:
-            response_mock.status_code = 500
-            response_mock.content = 'Error'
-            post.side_effect = Mock(return_value=response_mock)
-            response = CreateApp().post(request)
-            self.assertEqual('Error', response.context_data.get("errors"))
+        response_mock.status_code = 500
+        response_mock.content = 'Error'
+        post.side_effect = Mock(return_value=response_mock)
+        response = CreateApp().post(request)
+        self.assertEqual('Error', response.context_data.get("errors"))
 
     def test_post_without_name_should_return_form_with_errors(self):
         request = RequestFactory().post("/", {"name": ""})
@@ -42,29 +42,29 @@ class CreateAppViewTest(TestCase):
         self.assertIn('name', form.errors)
         self.assertIn(u'This field is required.', form.errors.get('name'))
 
-    def test_post_should_send_to_tsuru_with_args_expected(self):
+    @patch('requests.post')
+    def test_post_should_send_to_tsuru_with_args_expected(self, post):
         request = RequestFactory().post(
             "/",
             {"name": "myepe", "framework": "django"})
         request.session = {'tsuru_token': 'tokentest'}
-        with patch('requests.post') as post:
-            CreateApp().post(request)
-            self.assertEqual(1, post.call_count)
-            post.assert_called_with(
-                '%s/apps' % settings.TSURU_HOST,
-                data='{"framework": "django", "name": "myepe"}',
-                headers={'authorization': request.session['tsuru_token']}
-            )
+        CreateApp().post(request)
+        self.assertEqual(1, post.call_count)
+        post.assert_called_with(
+            '%s/apps' % settings.TSURU_HOST,
+            data='{"framework": "django", "name": "myepe"}',
+            headers={'authorization': request.session['tsuru_token']}
+        )
 
-    def test_invalid_post_returns_context_with_message_expected(self):
+    @patch('requests.post')
+    def test_invalid_post_returns_context_with_message_expected(self, post):
         request = RequestFactory().post(
             "/",
             {"name": "myepe", "framework": "python"})
         request.session = {}
         response_mock = Mock()
-        with patch('requests.post') as post:
-            response_mock.status_code = 200
-            post.side_effect = Mock(return_value=response_mock)
-            response = CreateApp().post(request)
-            self.assertEqual("App was successfully created",
-                             response.context_data.get('message'))
+        response_mock.status_code = 200
+        post.side_effect = Mock(return_value=response_mock)
+        response = CreateApp().post(request)
+        self.assertEqual("App was successfully created",
+                         response.context_data.get('message'))
