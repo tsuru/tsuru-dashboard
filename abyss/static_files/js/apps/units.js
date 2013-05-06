b59717905e3033762a0850ba871ffc301020efe4
Andrews Medina <andrewsmedina@gmail.com>
2013-05-06 19:14:42 -0300

    apps: added js to control units slider.
    related to #30 and #31.

---
+++ abyss/static_files/js/apps/units.js
@@ -0,0 +1,28 @@
+(function($) {
+
+	var Slider = function(units) {
+		this.units = units;
+		var self = this;
+
+		$('#h-slider').slider({
+			orientation: "horizontal",
+			value: this.units,
+			range: "min",
+			min: 1,
+			max: 10,
+			slide:  function( event, ui ) {
+				$( "#units" ).html( ui.value );
+				$( "input[name='units']" ).val( ui.value );
+			},
+			change: function( event, ui ) {
+				if (ui.value !== self.units) {
+					$(".apply-btn").show();
+				} else {
+					$(".apply-btn").hide();
+				}
+			}
+		});
+	}
+
+	$.Slider = Slider;
+})(jQuery);
