76b9851a2460bd1e53eaac6c2b204c88a815a92a
Andrews Medina <andrewsmedina@gmail.com>
2013-05-07 16:57:43 -0300

    added url for service add.
    related to #7.

--- services/urls.py
+++ services/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
 
-from services.views import ListService, ServiceDetail
+from services import views
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', ListService.as_view(), name='service-list'),
-    url(r'^(?P<service_name>[\w-]+)/$', ServiceDetail.as_view(),
+    url(r'^$', views.ListService.as_view(), name='service-list'),
+    url(r'^(?P<service_name>[\w-]+)/$', views.ServiceDetail.as_view(),
         name='service-detail'),
+    url(r'^(?P<service_name>[\w-]+)/add/$', views.ServiceAdd.as_view(),
+        name='service-add'),
 )
