f899448504836d3d520c1c673958f3723c6ba5b7
Andrews Medina <andrewsmedina@gmail.com>
2013-05-11 13:35:09 -0300

    list team: added view.
    related to #39.

---
+++ teams/tests/test_list_view.py
@@ -0,0 +1,26 @@
+from mock import patch, Mock
+
+from django.conf import settings
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+from teams.views import List
+
+
+class ListServiceViewTest(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get("/")
+        self.request.session = {"tsuru_token": "admin"}
+
+    @patch('requests.get')
+    def test_should_use_list_template(self, get):
+        response_mock = Mock()
+        response_mock.json.return_value = []
+        get.return_value = response_mock
+        response = List.as_view()(self.request)
+        self.assertEqual("teams/list.html", response.template_name)
+        expected = []
+        self.assertListEqual(expected, response.context_data['teams'])
+        get.assert_called_with(
+            '{0}/teams'.format(settings.TSURU_HOST),
+            headers={'authorization': 'admin'})
--- teams/views.py
+++ teams/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.template.response import TemplateResponse
+from django.conf import settings
+
+from auth.views import LoginRequiredView
+
+import requests
+
+
+class List(LoginRequiredView):
+    def get(self, request):
+        auth = {'authorization': request.session.get('tsuru_token')}
+        url = "{0}/teams".format(settings.TSURU_HOST)
+        teams = requests.get(url, headers=auth).json()
+        return TemplateResponse(request, "teams/list.html", {'teams': teams})
