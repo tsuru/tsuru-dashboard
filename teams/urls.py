a97fcaf94c60172fbc1d6818d1737102db473b9a
Andrews Medina <andrewsmedina@gmail.com>
2013-05-11 23:04:35 -0300

    team remove: added url.
    closes #40.

--- teams/urls.py
+++ teams/urls.py
@@ -6,4 +6,6 @@ from teams import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.List.as_view(), name='team-list'),
+    url(r'^(?P<team>[\w-]+)/remove/$', views.Remove.as_view(),
+        name='team-remove'),
 )
