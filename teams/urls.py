3a0d9a0a0f08bc9f75841951c91b760f0071da7c
Andrews Medina <andrewsmedina@gmail.com>
2013-05-12 00:30:36 -0300

    added team add view.

--- teams/urls.py
+++ teams/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns(
     url(r'^$', views.List.as_view(), name='team-list'),
     url(r'^(?P<team>[\w-]+)/remove/$', views.Remove.as_view(),
         name='team-remove'),
+    url(r'^add/$', views.Add.as_view(),
+        name='team-add'),
 )
--- teams/views.py
+++ teams/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 
 from auth.views import LoginRequiredView
+from teams.forms import TeamForm
 
 import requests
+import json
 
 
 class List(LoginRequiredView):
@@ -23,3 +25,25 @@ class Remove(LoginRequiredView):
         url = "{0}/teams/{1}".format(settings.TSURU_HOST, team_name)
         requests.delete(url, headers=auth)
         return redirect(reverse('team-list'))
+
+
+class Add(LoginRequiredView):
+
+    def get(self, request):
+        return TemplateResponse(request, 'auth/team.html',
+                                {"form": TeamForm()})
+
+    def post(self, request):
+        form = TeamForm(request.POST)
+        errors = ''
+        if form.is_valid():
+            authorization = {'authorization':
+                             request.session.get('tsuru_token')}
+            response = requests.post('{0}/teams'.format(settings.TSURU_HOST),
+                                     data=json.dumps(form.data),
+                                     headers=authorization)
+            if response.status_code == 200:
+                return redirect(reverse("team-list"))
+            errors = response.content
+        return TemplateResponse(request, 'auth/team.html',
+                                {'form': form, 'errors': errors})
