afcf069f413331925a15664bc9dc358792f6a1ab
Andrews Medina <andrewsmedina@gmail.com>
2013-05-13 16:55:09 -0300

    team info: added url.
    related to #43.

--- teams/urls.py
+++ teams/urls.py
@@ -6,6 +6,7 @@ from teams import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.List.as_view(), name='team-list'),
+    url(r'^(?P<team>[\w-]+)/$', views.Info.as_view(), name='team-info'),
     url(r'^(?P<team>[\w-]+)/remove/$', views.Remove.as_view(),
         name='team-remove'),
     url(r'^add/$', views.Add.as_view(),
