ba3bc6b5aaecf5d0df8e63ef7346f86981b0a09d
Andrews Medina <andrewsmedina@gmail.com>
2013-05-14 16:53:34 -0300

    teams: fixed url order.

--- teams/urls.py
+++ teams/urls.py
@@ -6,11 +6,11 @@ from teams import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.List.as_view(), name='team-list'),
-    url(r'^(?P<team>[\w-]+)/$', views.Info.as_view(), name='team-info'),
     url(r'^(?P<team>[\w-]+)/remove/$', views.Remove.as_view(),
         name='team-remove'),
     url(r'^add/$', views.Add.as_view(),
         name='team-add'),
     url(r'^(?P<team>[\w-]+)/user/add/$', views.AddUser.as_view(),
         name='team-user-add'),
+    url(r'^(?P<team>[\w-]+)/$', views.Info.as_view(), name='team-info'),
 )
