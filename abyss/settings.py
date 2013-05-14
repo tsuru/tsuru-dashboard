f731c00c87292122d00a9ead7ba944e3397007cd
Andrews Medina <andrewsmedina@gmail.com>
2013-05-14 17:39:11 -0300

    added middleware for localization.
    related to #44.

--- abyss/settings.py
+++ abyss/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 ROOT_URLCONF = 'abyss.urls'
