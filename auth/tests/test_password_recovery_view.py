6017ee056e8de9e9aa8c30ececc1f6fe16229681
Andrews Medina <andrewsmedina@gmail.com>
2013-05-16 17:12:36 -0300

    sending password recovery on post.
    related to #33.

--- auth/tests/test_password_recovery_view.py
+++ auth/tests/test_password_recovery_view.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from auth.forms import PasswordRecoveryForm
+
+from mock import patch
 
 
 class PasswordRecoveryViewTest(TestCase):
@@ -10,3 +13,15 @@ class PasswordRecoveryViewTest(TestCase):
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed('auth/password_recovery.html')
         self.assertIsInstance(response.context['form'], PasswordRecoveryForm)
+
+    @patch("requests.post")
+    def test_send_when_form_is_valid(self, post):
+        self.client.post(reverse('password-recovery'),
+                         {"email": "some@email.com", "token": "tt"})
+        # self.assertRedirects(response, "/")
+        url = "{0}/users/{1}/password?token={2}".format(
+            settings.TSURU_HOST,
+            "some@email.com",
+            "tt"
+        )
+        post.assert_called_with(url)
--- auth/views.py
+++ auth/views.py
@@ -41,6 +41,10 @@ class PasswordRecovery(FormView):
     form_class = PasswordRecoveryForm
     success_url = '/'
 
+    def form_valid(self, form):
+        form.send()
+        return super(PasswordRecovery, self).form_valid(form)
+
 
 class Login(View):
 
