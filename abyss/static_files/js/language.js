97f009311d31ff4ca7b1024a78a137b9ac0bec76
Andrews Medina <andrewsmedina@gmail.com>
2013-05-20 18:56:58 -0300

    improved the cookies manager.

--- abyss/static_files/js/language.js
+++ abyss/static_files/js/language.js
@@ -1,7 +1,29 @@
 (function(window, $) {
+	function Cookie(options) {
+		this.options = options;
+	}
+
+	Cookie.prototype.remove = function() {
+		var options = this.options.constructor();
+		options.key = this.options.key;
+		options.duration = -1;
+		new Cookie(options).write();
+	}
+
+	Cookie.prototype.write = function(value) {
+		if (this.options.duration){
+			var date = new Date();
+			date.setTime(date.getTime() + this.options.duration * 24 * 60 * 60 * 1000);
+			value += '; expires=' + date.toGMTString();
+		}
+		window.document.cookie = this.options.key + '=' + value;
+	}
+
 	$(".language-select a").on('click', function(event) {
 		event.preventDefault();
-		window.document.cookie = "django_language=" + $(this).attr('href');
+		var cookie = new Cookie({"key": "django_language"});
+		cookie.remove();
+		cookie.write($(this).attr("href"));
 		window.location.reload();
 	});
 })(window, jQuery);
