97096df824936fbb35aa902d6ff671bd79fa2913
Andrews Medina <andrewsmedina@gmail.com>
2013-05-21 17:51:48 -0300

    extract pipeline to a js file.

---
+++ abyss/static_files/js/pipeline.js
@@ -0,0 +1,28 @@
+(function(window, $) {
+
+	function next() {
+		var $container = $(".active");
+		$container.removeClass("active");
+		$container.next().addClass("active");
+	}
+
+	function Pipeline(actions) {
+		this.actions = actions;
+	}
+
+	Pipeline.prototype.next = function() {
+		if (this.actions.length > 0) {
+			var action = this.actions.pop();
+			action.call()
+		}
+	}
+
+	var actions = [next, next];
+
+	var pipeline = new Pipeline(actions);
+
+	$(".next").on("click", function(event) {
+		pipeline.next();
+		return false;
+	});
+})(window, $)
