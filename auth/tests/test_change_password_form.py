224ac8a5575412b213d4b0d2e72002294c6440e6
Andrews Medina <andrewsmedina@gmail.com>
2013-05-24 13:07:59 -0300

    change password: added tests for is required validations.
    related to #46.

--- auth/tests/test_change_password_form.py
+++ auth/tests/test_change_password_form.py
@@ -12,3 +12,27 @@ class ChangePasswordFormTest(TestCase):
         }
         form = ChangePasswordForm(data)
         self.assertTrue(form.is_valid())
+
+    def test_old_is_required(self):
+        data = {
+            "new": "new",
+            "confirm": "new",
+        }
+        form = ChangePasswordForm(data)
+        self.assertFalse(form.is_valid())
+
+    def test_new_is_required(self):
+        data = {
+            "old": "old",
+            "confirm": "new",
+        }
+        form = ChangePasswordForm(data)
+        self.assertFalse(form.is_valid())
+
+    def test_confirm_is_required(self):
+        data = {
+            "old": "old",
+            "new": "new",
+        }
+        form = ChangePasswordForm(data)
+        self.assertFalse(form.is_valid())
