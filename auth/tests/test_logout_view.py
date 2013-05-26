b01ecc328af1ce7ca86ea3ee22b58756df336a89
Andrews Medina <andrewsmedina@gmail.com>
2013-05-26 13:04:16 -0300

    refactored logout test.

--- auth/tests/test_logout_view.py
+++ auth/tests/test_logout_view.py
@@ -5,25 +5,27 @@ from django.test.client import RequestFactory
 from auth.views import Logout
 
 
-class LogoutViewTest(TestCase):
+class TestLogoutWhenUserIsLoggedIn(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get('/logout/')
+        self.request.session = {'tsuru_token': 'my beautiful token'}
+        self.response = Logout.as_view()(self.request)
 
     def test_should_remove_tsuru_token_from_session(self):
-        request = RequestFactory().get('/logout/')
-        request.session = {'tsuru_token': 'my beautiful token'}
-        Logout().get(request)
-        token = request.session.get('tsuru_token')
+        token = self.request.session.get('tsuru_token')
         self.assertIsNone(token)
 
     def test_should_redirect_to_index(self):
-        request = RequestFactory().get('/logout/')
-        request.session = {'tsuru_token': 'my beautiful token'}
-        response = Logout().get(request)
-        self.assertIsInstance(response, HttpResponseRedirect)
-        self.assertEqual('/', response['Location'])
+        self.assertIsInstance(self.response, HttpResponseRedirect)
+        self.assertEqual('/', self.response['Location'])
 
-    def test_should_redirect_to_index_if_the_user_is_not_logged_in(self):
-        request = RequestFactory().get('/logout/')
-        request.session = {}
-        response = Logout().get(request)
-        self.assertIsInstance(response, HttpResponseRedirect)
-        self.assertEqual('/', response['Location'])
+
+class TestLogoutWhenUserIsLogout(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get('/logout/')
+        self.request.session = {}
+        self.response = Logout.as_view()(self.request)
+
+    def test_should_redirect_to_index(self):
+        self.assertIsInstance(self.response, HttpResponseRedirect)
+        self.assertEqual('/', self.response['Location'])
