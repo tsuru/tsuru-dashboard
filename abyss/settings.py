a066ad19db323ed18f4823962eaffccabe24e5dd
Andrews Medina <andrewsmedina@gmail.com>
2014-07-25 17:49:26 -0300

    auth: force logout when token is expired.

--- abyss/settings.py
+++ abyss/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'auth.middleware.VerifyToken',
 )
 
 ROOT_URLCONF = 'abyss.urls'
