cd0c7b0ad0c0d122821a74cbb9faf1f4a9ebc623
Andrews Medina <andrewsmedina@gmail.com>
2014-08-04 15:17:29 -0300

    auth: logging errors.

--- auth/middleware.py
+++ auth/middleware.py
@@ -1,8 +1,13 @@
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class VerifyToken(object):
     def process_exception(self, request, exception): 
         if isinstance(exception, Exception):
+            logging.error(exception)
             return redirect(reverse("logout"))
