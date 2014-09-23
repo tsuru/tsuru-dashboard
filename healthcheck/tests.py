6c8b972ddc3d6024cc5a0932cb84bc7bf1066b35
Andrews Medina <andrewsmedina@gmail.com>
2014-09-23 11:34:43 -0300

    healthcheck: check connection with tsuru.
    closes #81.

--- healthcheck/tests.py
+++ healthcheck/tests.py
@@ -1,9 +1,44 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+import mock
+import requests
+
 
 class HealthcheckTest(TestCase):
-    def test_view(self):
+
+    @mock.patch("requests.get")
+    def test_healthcheck_ok(self, get_mock):
+        get_mock.return_value = mock.Mock(status_code=200, text="WORKING")
+
         response = self.client.get(reverse("healthcheck"))
+
         self.assertEqual(200, response.status_code)
         self.assertEqual("WORKING", response.content)
+
+    @mock.patch("requests.get")
+    def test_healthcheck_connection_error(self, get_mock):
+        get_mock.side_effect = requests.exceptions.ConnectionError()
+
+        response = self.client.get(reverse("healthcheck"))
+
+        self.assertEqual(500, response.status_code)
+        self.assertEqual("Failed to connect to tsuru.", response.content)
+
+    @mock.patch("requests.get")
+    def test_healthcheck_wrong_status_code(self, get_mock):
+        get_mock.return_value = mock.Mock(status_code=500, text="WORKING")
+
+        response = self.client.get(reverse("healthcheck"))
+
+        self.assertEqual(500, response.status_code)
+        self.assertEqual("Failed to connect to tsuru.", response.content)
+
+    @mock.patch("requests.get")
+    def test_healthcheck_wrong_text(self, get_mock):
+        get_mock.return_value = mock.Mock(status_code=200, text="not expected")
+
+        response = self.client.get(reverse("healthcheck"))
+
+        self.assertEqual(500, response.status_code)
+        self.assertEqual("Failed to connect to tsuru.", response.content)
--- healthcheck/views.py
+++ healthcheck/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.conf import settings
+
+import requests
 
 
 def healthcheck(request):
+    url = "{0}/healthcheck/".format(settings.TSURU_HOST)
+
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        return HttpResponse("Failed to connect to tsuru.", status=500)
+
+    if response.status_code != 200 or response.text != "WORKING":
+        return HttpResponse("Failed to connect to tsuru.", status=500)
+
     return HttpResponse("WORKING", status=200)
