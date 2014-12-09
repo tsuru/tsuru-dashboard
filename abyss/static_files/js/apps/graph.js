73b32879545a040ba0e71a15ea60117a83faa920
Andrews Medina <andrewsmedina@gmail.com>
2014-12-09 19:47:00 -0200

    metrics: add suport for net connections.

--- abyss/static_files/js/apps/graph.js
+++ abyss/static_files/js/apps/graph.js
@@ -5,29 +5,32 @@
 		if ( host.indexOf( "http" ) === -1 ) {
 			host = "http://" + host;
 		}
-		var url = host + "/render/?target=summarize(maxSeries(keepLastValue(statsite.tsuru." + opts["appName"] + ".*.*." + opts["kind"] + ")), \"" + opts["serie"] + "\", \"max\")&format=json&jsonp=?&from=-" + opts["from"];
+		var url = host + "/render/?target=summarize(maxSeries(keepLastValue(statsite.tsuru." + opts["appName"] + "." + opts["kind"] + ")), \"" + opts["serie"] + "\", \"max\")&format=json&jsonp=?&from=-" + opts["from"];
 		$.getJSON( url , function( data ) {
 			var d = [];
 			$.each(data, function(index, target) {
 				$.each(target["datapoints"], function(index, value) {
 					var v = value[0];
-					if ( opts["kind"] === "mem_max" ) {
+					if ( opts["kind"].indexOf("mem_max") !== -1 ) {
 						v = v / ( 1024 * 1024 );
+					}
+					if ( opts["kind"].indexOf("disk_usage") !== -1 ) {
+						v = v / ( 1024 * 1024 * 1024 );
 					}
 					d.push({y: v, x: new Date(value[1] * 1000).getTime()});
 				});
 			});
 
-			if ( $("#" + opts["kind"]).parents(".graph-container").length === 0 ) {
+			if ( $("#" + opts["id"]).parents(".graph-container").length === 0 ) {
 				var label = opts["label"] ? opts["label"] : opts["kind"];
 				var url = '/apps/' + opts["appName"] + '/metrics/?kind=' + opts["kind"] + '&from=' + opts["from"] + '&serie=' + opts["serie"];
-				var element = '<div class="graph-container"><h2>' + label + '</h2><a href="' + url + '"><div id="' + opts["kind"] + '"></div></a></div>';
+				var element = '<div class="graph-container"><h2>' + label + '</h2><a href="' + url + '"><div id="' + opts["id"] + '"></div></a></div>';
 				$( '#metrics' ).append(element);
 			} else {
-				$("#" + opts["kind"]).html("");
+				$("#" + opts["id"]).html("");
 			}
 			var options = {
-				element: opts["kind"],
+				element: opts["id"],
 				pointSize: 0,
 				smooth: true,
 				data: d,
