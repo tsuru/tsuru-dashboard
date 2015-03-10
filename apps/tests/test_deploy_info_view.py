9ccc0e4151f6a8b79da44ec3a4132f056b6104b5
Francisco Souza <f@souza.cc>
2015-03-10 16:00:09 -0300

    apps/tests: fix tests
    Using just self.data or data would actually change the dictionary, and
    assertEqual would NEVER fail. Now copying the value, so it works as
    expected.

--- apps/tests/test_deploy_info_view.py
+++ apps/tests/test_deploy_info_view.py
@@ -25,15 +25,17 @@ class InfoViewTest(TestCase):
     @patch("requests.get")
     def test_view(self, get):
         response_mock = Mock()
-        response_mock.json.return_value = self.data
+        response_mock.json.return_value = copy.deepcopy(self.data)
         get.return_value = response_mock
         response = DeployInfo.as_view()(
             self.request,
             deploy="53e143cb874ccb1f68000001",
             app_name="app_name",
         )
+        expected = copy.deepcopy(self.data)
+        expected["Diff"] = u"""<div class="highlight"><pre>%s\n</pre></div>\n""" % self.data["Diff"]
         self.assertEqual("apps/deploy.html", response.template_name[0])
-        self.assertDictEqual(self.data, response.context_data['deploy'])
+        self.assertDictEqual(expected, response.context_data['deploy'])
         self.assertIn('app', response.context_data.keys())
         get.assert_called_with(
             '{0}/deploys/{1}'.format(settings.TSURU_HOST,
@@ -46,13 +48,14 @@ class InfoViewTest(TestCase):
         data = copy.deepcopy(self.data)
         del data["Diff"]
         response_mock = Mock()
-        response_mock.json.return_value = data
+        response_mock.json.return_value = copy.deepcopy(data)
         get.return_value = response_mock
         response = DeployInfo.as_view()(
             self.request,
             deploy="53e143cb874ccb1f68000001",
             app_name="app_name",
         )
+        data["Diff"] = u"""<div class="highlight"><pre>\n</pre></div>\n"""
         self.assertEqual("apps/deploy.html", response.template_name[0])
         self.assertDictEqual(data, response.context_data['deploy'])
         self.assertIn('app', response.context_data.keys())
