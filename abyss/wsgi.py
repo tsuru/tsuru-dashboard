2e5017ed09493a4391ceaaa8b4386a28cb9c8b20
Francisco Souza <f@souza.cc>
2015-03-10 10:01:15 -0300

    abyss/wsgi: fix flake8 issue

--- abyss/wsgi.py
+++ abyss/wsgi.py
@@ -1,6 +1,7 @@
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abyss.settings")
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
