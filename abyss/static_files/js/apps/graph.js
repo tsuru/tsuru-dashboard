f8a0fce90f693aeaf201be9f3a8618c402875582
Andrews Medina <andrewsmedina@gmail.com>
2015-03-11 17:51:22 -0300

    js/graph: extract some functions.

--- abyss/static_files/js/apps/graph.js
+++ abyss/static_files/js/apps/graph.js
@@ -1,10 +1,68 @@
 (function($, window){
 
-	var normalizeUrl = function(host) {
+  	var normalizeUrl = function(host) {
 		if (host.indexOf("http") === -1) {
 			host = "http://" + host;
 		}
 		return host;
+	}
+
+	var buildQuery = function(opts) {
+		return opts["graphiteHost"] + "/render/?target=summarize(" + opts["statistic"] + "(keepLastValue(" + opts["prefix"] + "." + opts["kind"] + ")), \"" + opts["serie"] + "\", \"max\")&format=json&jsonp=?&from=-" + opts["from"];
+	}
+
+	var processData = function(opts, data) {
+		var d = [];
+
+		$.each(data, function(index, target) {
+			$.each(target["datapoints"], function(index, value) {
+				var v = value[0];
+
+				if ((opts["kind"].indexOf("mem_max") !== -1 ) ||
+					(opts["kind"].indexOf("recv") !== -1) ||
+					(opts["kind"].indexOf("sent") !== -1 )) {
+
+					v = v / ( 1024 * 1024 );
+
+				}
+
+				d.push({y: v, x: new Date(value[1] * 1000).getTime()});
+			});
+		});
+
+		return d;
+	}
+
+	var buildGraph = function(opts, data) {
+
+		var options = {
+			element: opts["id"],
+			pointSize: 0,
+			smooth: true,
+			data: processData(opts, data),
+			xkey: 'x',
+			ykeys: ['y'],
+			ymax: opts["max"],
+			labels: ['Value']
+		};
+
+		if (!opts["hover"]) {
+			options["hideHover"] = "always";
+		}
+
+		buildContainer(opts);
+		new Morris.Line(options);
+	}
+
+	var buildContainer = function(opts) {
+		if ( $("#" + opts["id"]).parents(".graph-container").length === 0 ) {
+			var label = opts["label"] ? opts["label"] : opts["kind"];
+			var url = '/apps/' + opts["appName"] + '/metrics/?kind=' + opts["kind"] + '&from=' + opts["from"] + '&serie=' + opts["serie"];
+			var element = '<div class="graph-container"><h2>' + label + '</h2><a href="' + url + '"><div id="' + opts["id"] + '"></div></a></div>';
+			$( '#metrics' ).append(element);
+		} else {
+			$("#" + opts["id"]).html("");
+		}
 	}
 
 	var graph = function(opts) {
@@ -15,52 +73,22 @@
 
 		opts["graphiteHost"] = normalizeUrl(opts["graphiteHost"]);
 
-		var url = opts["graphiteHost"] + "/render/?target=summarize(" + opts["statistic"] + "(keepLastValue(" + opts["prefix"] + "." + opts["kind"] + ")), \"" + opts["serie"] + "\", \"max\")&format=json&jsonp=?&from=-" + opts["from"];
+		var url = buildQuery(opts);
 
-		$.getJSON( url , function( data ) {
-			var d = [];
-			$.each(data, function(index, target) {
-				$.each(target["datapoints"], function(index, value) {
-					var v = value[0];
-					if ( ( opts["kind"].indexOf("mem_max") !== -1 ) || ( opts["kind"].indexOf("recv") !== -1 ) || ( opts["kind"].indexOf("sent") !== -1 ) ) {
-						v = v / ( 1024 * 1024 );
-					}
-					d.push({y: v, x: new Date(value[1] * 1000).getTime()});
-				});
-			});
+		$.getJSON(url , function(data) {
 
-			if ( $("#" + opts["id"]).parents(".graph-container").length === 0 ) {
-				var label = opts["label"] ? opts["label"] : opts["kind"];
-				var url = '/apps/' + opts["appName"] + '/metrics/?kind=' + opts["kind"] + '&from=' + opts["from"] + '&serie=' + opts["serie"];
-				var element = '<div class="graph-container"><h2>' + label + '</h2><a href="' + url + '"><div id="' + opts["id"] + '"></div></a></div>';
-				$( '#metrics' ).append(element);
-			} else {
-				$("#" + opts["id"]).html("");
-			}
-			var options = {
-				element: opts["id"],
-				pointSize: 0,
-				smooth: true,
-				data: d,
-				xkey: 'x',
-				ykeys: ['y'],
-				ymax: opts["max"],
-				labels: ['Value']
-			};
-			if (!opts["hover"]) {
-				options["hideHover"] = "always";
-			}
-			new Morris.Line(options);
+			buildGraph(opts, data);
 			window.setTimeout(graph, 60000, opts);
 
+
 		});
-	}
+}
 
-	var graphs = function(opts) {
-		graph(opts);
-	}
+var graphs = function(opts) {
+	graph(opts);
+}
 
-	$.Graph = graphs;
+$.Graph = graphs;
 
 
 })(jQuery, window);
