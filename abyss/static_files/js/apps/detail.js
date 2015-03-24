00490a467828053789d349eb125e8f8df6109c55
Andrews Medina <andrewsmedina@gmail.com>
2015-03-24 18:23:01 -0300

    app/detail: remove specific logic from graph.js.

--- abyss/static_files/js/apps/detail.js
+++ abyss/static_files/js/apps/detail.js
@@ -1,7 +1,29 @@
 (function($, window){
 
 	var detail = function(appName, envs) {
-		$.AllGraphs(appName, envs);
+		var allGraphs = function(appName, envs) {
+			var kinds = ["mem_max", "cpu_max", "connections"];
+
+			if (envs.hasOwnProperty("ELASTICSEARCH_HOST")){
+				kinds.push("response_time");
+				kinds.push("requests_min");
+				kinds.push("units");
+			}
+
+			$.each(kinds, function(i, kind) {
+				var opts = {
+					appName: appName,
+					serie: "1min",
+					from: "1h",
+					hover: false,
+					kind: kind,
+					envs: envs
+				}
+				$.Graph(opts);
+			});
+		};
+
+		allGraphs(appName, envs);
 		$.confirmation(".btn-remove", ".remove-confirmation", appName);
 	};
 
--- abyss/static_files/js/apps/graph.js
+++ abyss/static_files/js/apps/graph.js
@@ -10,29 +10,6 @@
 		}
 	}
 
-	var allGraphs = function(appName, envs) {
-		var kinds = ["mem_max", "cpu_max", "connections"];
-
-		if (envs.hasOwnProperty("ELASTICSEARCH_HOST")){
-			kinds.push("response_time");
-			kinds.push("requests_min");
-			kinds.push("units");
-		}
-
-		$.each(kinds, function(i, kind) {
-			var opts = {
-				appName: appName,
-				serie: "1min",
-				from: "1h",
-				hover: false,
-				kind: kind,
-				envs: envs
-			}
-			graph(opts);
-		});
-	};
-
-	$.AllGraphs = allGraphs;
 	$.Graph = graph;
 
 })(jQuery, window);
