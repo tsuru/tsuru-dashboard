80d89d6213c8f058235d7e2394d6907a2b67ded2
Andrews Medina <andrewsmedina@gmail.com>
2015-05-12 20:31:24 -0300

    autoscale: embedding autoscale page.

--- autoscale/views.py
+++ autoscale/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+import os
+
 
 def index(request):
-    return render(request, "autoscale/index.html")
+    context = {
+        "service_url": os.environ.get("AUTOSCALE_DASHBOARD_URL"),
+    }
+    return render(request, "autoscale/index.html", context)
