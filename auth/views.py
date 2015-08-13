32c24d6474b07bc9c22bf56d89d6e4b49461f842
Andrews Medina <andrewsmedina@gmail.com>
2015-08-13 12:40:15 -0300

    auth/keys: using LoginRequiredMixin authorization method

--- auth/views.py
+++ auth/views.py
@@ -192,11 +192,8 @@ class Key(LoginRequiredMixin, FormView):
     success_url = '/auth/key/'
 
     def form_valid(self, form):
-        authorization = {'authorization':
-                         self.request.session.get('tsuru_token')}
-        response = requests.post('%s/users/keys' % settings.TSURU_HOST,
-                                 data=json.dumps(form.cleaned_data),
-                                 headers=authorization)
+        url = '{}/users/keys'.format(settings.TSURU_HOST)
+        response = requests.post(url, data=json.dumps(form.cleaned_data), headers=self.authorization)
         if response.status_code < 399:
             messages.success(self.request, "The key was successfully added", fail_silently=True)
         else:
