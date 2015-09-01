4aac0071d97585c5d38d51ccdab9c10559343da6
Andrews Medina <andrewsmedina@gmail.com>
2015-08-31 22:31:26 -0300

    teams/view: use authorization headers from LoginRequiredView

--- teams/tests/test_list_view.py
+++ teams/tests/test_list_view.py
@@ -7,7 +7,7 @@ from django.test.client import RequestFactory
 from teams.views import List
 
 
-class ListServiceViewTest(TestCase):
+class ListTeamViewTest(TestCase):
     def setUp(self):
         self.request = RequestFactory().get("/")
         self.request.session = {"tsuru_token": "admin"}
@@ -17,12 +17,14 @@ class ListServiceViewTest(TestCase):
         response_mock = Mock(status_code=200)
         response_mock.json.return_value = []
         get.return_value = response_mock
+
         response = List.as_view()(self.request)
+
         self.assertEqual("teams/list.html", response.template_name)
         expected = []
         self.assertListEqual(expected, response.context_data['teams'])
         get.assert_called_with(
-            '{0}/teams'.format(settings.TSURU_HOST),
+            '{}/teams'.format(settings.TSURU_HOST),
             headers={'authorization': 'admin'})
 
     @patch('requests.get')
@@ -30,10 +32,12 @@ class ListServiceViewTest(TestCase):
     def test_should_return_empty_list_when_status_is_204(self, token_is_valid, get):
         token_is_valid.return_value = True
         get.return_value = Mock(status_code=204)
+
         response = List.as_view()(self.request)
+
         self.assertEqual("teams/list.html", response.template_name)
         expected = []
         self.assertListEqual(expected, response.context_data['teams'])
         get.assert_called_with(
-            '{0}/teams'.format(settings.TSURU_HOST),
+            '{}/teams'.format(settings.TSURU_HOST),
             headers={'authorization': 'admin'})
--- teams/views.py
+++ teams/views.py
@@ -15,24 +15,23 @@ class RemoveUser(LoginRequiredView):
     def get(self, request, *args, **kwargs):
         team_name = kwargs["team"]
         user = kwargs["user"]
-        headers = {'authorization': request.session.get('tsuru_token')}
-        url = "{0}/teams/{1}/{2}".format(settings.TSURU_HOST, team_name, user)
-        response = requests.delete(url, headers=headers)
+        url = "{}/teams/{}/{}".format(settings.TSURU_HOST, team_name, user)
+        response = requests.delete(url, headers=self.authorization)
+
         if response.status_code < 399:
             messages.success(self.request, u'User successfully removed!', fail_silently=True)
         else:
             messages.error(self.request, response.text, fail_silently=True)
+
         return redirect(reverse('team-info', args=[team_name]))
 
 
 class AddUser(LoginRequiredView):
     def post(self, request, *args, **kwargs):
         team_name = kwargs["team"]
-        headers = {'authorization': request.session.get('tsuru_token')}
         user = request.POST["user"]
-        url = "{0}/teams/{1}/{2}".format(settings.TSURU_HOST, team_name,
-                                         user)
-        response = requests.put(url, headers=headers)
+        url = "{0}/teams/{1}/{2}".format(settings.TSURU_HOST, team_name, user)
+        response = requests.put(url, headers=self.authorization)
         if response.status_code != 200:
             messages.error(self.request, response.text, fail_silently=True)
         return redirect(reverse('team-info', args=[team_name]))
@@ -41,9 +40,8 @@ class AddUser(LoginRequiredView):
 class Info(LoginRequiredView):
     def get(self, request, *args, **kwargs):
         team_name = kwargs["team"]
-        headers = {'authorization': request.session.get('tsuru_token')}
         url = "{0}/teams/{1}".format(settings.TSURU_HOST, team_name)
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=self.authorization)
         context = {"team": response.json()}
         return TemplateResponse(request, "teams/info.html", context)
 
@@ -73,22 +71,16 @@ class Remove(LoginRequiredView):
 
 
 class Add(LoginRequiredView):
-
     def get(self, request):
-        return TemplateResponse(request, 'auth/team.html',
-                                {"form": TeamForm()})
+        return TemplateResponse(request, 'auth/team.html', {"form": TeamForm()})
 
     def post(self, request):
         form = TeamForm(request.POST)
         errors = ''
         if form.is_valid():
-            authorization = {'authorization':
-                             request.session.get('tsuru_token')}
-            response = requests.post('{0}/teams'.format(settings.TSURU_HOST),
-                                     data=json.dumps(form.data),
-                                     headers=authorization)
+            url = '{}/teams'.format(settings.TSURU_HOST)
+            response = requests.post(url, data=json.dumps(form.data), headers=self.authorization)
             if response.status_code == 200:
                 return redirect(reverse("team-list"))
             errors = response.content
-        return TemplateResponse(request, 'auth/team.html',
-                                {'form': form, 'errors': errors})
+        return TemplateResponse(request, 'auth/team.html', {'form': form, 'errors': errors})
