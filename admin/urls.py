53cb422b71f98987e6fe83d972547b5efb865a9d
Andrews Medina <andrewsmedina@gmail.com>
2015-11-16 14:13:36 -0200

    admin: change the pool list page cache time

--- admin/urls.py
+++ admin/urls.py
@@ -5,9 +5,8 @@ from admin import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', cache_page(60 * 5)(views.PoolList.as_view()), name='pool-list'),
+    url(r'^$', cache_page(60 * 1)(views.PoolList.as_view()), name='pool-list'),
     url(r'^pool/(?P<pool>[\w-]+)/$', views.PoolInfo.as_view(), name='pool-info'),
-    url(r'^healing/$', views.ListHealing.as_view(), name='list-healing'),
     url(r'^(?P<address>[http://\w.:1-9-]+)/containers/$', views.ListContainer.as_view(),
         name='list-container'),
     url(r'^node/(?P<address>[http://\w.:1-9-]+)/remove/$', views.NodeRemove.as_view(),
@@ -15,4 +14,5 @@ urlpatterns = patterns(
     url(r'^deploys/$', views.ListDeploy.as_view(), name='list-deploys'),
     url(r'^deploys/graph$', views.DeploysGraph.as_view(), name='deploys-graph'),
     url(r'^deploys/(?P<deploy>[\s\w@\.-]+)/$', views.DeployInfo.as_view(), name='deploy-info'),
+    url(r'^healing/$', views.ListHealing.as_view(), name='list-healing'),
 )
