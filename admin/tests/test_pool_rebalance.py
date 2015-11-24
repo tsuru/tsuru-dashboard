25e1b36be352ce3e7300053afdf6281a59beee53
Andrews Medina <andrewsmedina@gmail.com>
2015-11-24 17:07:07 -0200

    add test for pool rebalance

---
+++ admin/tests/test_pool_rebalance.py
@@ -0,0 +1,38 @@
+from mock import patch
+
+from django.test import TestCase
+from django.test.client import RequestFactory
+from django.conf import settings
+
+import httpretty
+
+from admin.views import PoolRebalance
+
+
+class PoolRebalanceTest(TestCase):
+    def setUp(self):
+        httpretty.enable()
+
+    def tearDown(self):
+        httpretty.disable()
+        httpretty.reset()
+
+    @patch("auth.views.token_is_valid")
+    def test_view(self, token_is_valid):
+        token_is_valid.return_value = True
+
+        factory = RequestFactory()
+        request = factory.post('/')
+        request.session = {'tsuru_token': 'tokentest'}
+
+        url = "{}/docker/containers/rebalance".format(settings.TSURU_HOST)
+        httpretty.register_uri(
+            httpretty.POST,
+            url,
+            body="{}",
+            status=200
+        )
+
+        response = PoolRebalance.as_view()(request, pool="mypool")
+
+        self.assertEqual(response.status_code, 200)
