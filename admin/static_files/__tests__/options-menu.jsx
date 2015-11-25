0f8017b910c1cb595c2580c1d8f614d612c92978
Andrews Medina <andrewsmedina@gmail.com>
2015-11-25 19:34:57 -0200

    add option-menu react component

---
+++ admin/static_files/__tests__/options-menu.jsx
@@ -0,0 +1,16 @@
+jest.dontMock('../jsx/components/options-menu.jsx');
+
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    OptionsMenu = require('../jsx/components/options-menu.jsx'),
+    TestUtils = require('react-addons-test-utils');
+
+describe('OptionMenu', function() {
+  it('should has options-menu as className', function() {
+    var optionsMenu = TestUtils.renderIntoDocument(
+      <OptionsMenu />
+    );
+
+    expect(ReactDOM.findDOMNode(optionsMenu).className).toEqual("options-menu");
+  });
+});
---
+++ admin/static_files/jsx/components/options-menu.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var OptionsMenu = React.createClass({
+  render: function() {
+    return (
+      <div className='options-menu'>
+        <a href="#">...</a>
+      </div>
+    )
+  }
+});
+
+module.exports = OptionsMenu;
