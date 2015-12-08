0cae8eb346b017792618cdd0d73cc801e760e6b7
Andrews Medina <andrewsmedina@gmail.com>
2015-12-08 16:37:15 -0200

    setup: fix dependencie and ignore build dirs

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "Django>=1.9",
-        "request>=2.8.1",
+        "requests>=2.8.1",
         "python-dateutil>=2.4.2", 
         "pytz>=2015.4",
         "Pygments>=2.0.2",
