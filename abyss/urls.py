d931b40d2bbd6b46d829aec152c0f3df3a2e5c6f
Andrews Medina <andrewsmedina@gmail.com>
2015-12-10 15:45:09 -0200

    add urls.py in tsuru_dashboard app

--- abyss/urls.py
+++ abyss/urls.py
@@ -1,17 +1,5 @@
-from django.conf.urls import patterns, include
+from django.conf.urls import url, include
 
-from tsuru_dashboard.dashboard.views import IndexView
-
-urlpatterns = patterns(
-    '',
-    (r'^$', IndexView.as_view()),
-    (r'^admin/', include('tsuru_dashboard.admin.urls')),
-    (r'^auth/', include('tsuru_dashboard.auth.urls')),
-    (r'^apps/', include('tsuru_dashboard.apps.urls')),
-    (r'^services/', include('tsuru_dashboard.services.urls')),
-    (r'^teams/', include('tsuru_dashboard.teams.urls')),
-    (r'^healthcheck/', include('tsuru_dashboard.healthcheck.urls')),
-    (r'^autoscale/', include('tsuru_dashboard.autoscale.urls')),
-    (r'^dashboard/', include('tsuru_dashboard.dashboard.urls')),
-    (r'^metrics/', include('tsuru_dashboard.metrics.urls')),
-)
+urlpatterns = [
+    url(r'^', include('tsuru_dashboard.urls')),
+]
---
+++ tsuru_dashboard/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+
+from tsuru_dashboard.dashboard.views import IndexView
+
+urlpatterns = [
+    url(r'^$', IndexView.as_view()),
+    url(r'^admin/', include('tsuru_dashboard.admin.urls')),
+    url(r'^auth/', include('tsuru_dashboard.auth.urls')),
+    url(r'^apps/', include('tsuru_dashboard.apps.urls')),
+    url(r'^services/', include('tsuru_dashboard.services.urls')),
+    url(r'^teams/', include('tsuru_dashboard.teams.urls')),
+    url(r'^healthcheck/', include('tsuru_dashboard.healthcheck.urls')),
+    url(r'^autoscale/', include('tsuru_dashboard.autoscale.urls')),
+    url(r'^dashboard/', include('tsuru_dashboard.dashboard.urls')),
+    url(r'^metrics/', include('tsuru_dashboard.metrics.urls')),
+]
