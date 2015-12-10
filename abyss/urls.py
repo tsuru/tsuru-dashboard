ae23e7b7fc59dc0a085955eb4497a2298bdcaaf0
Andrews Medina <andrewsmedina@gmail.com>
2015-12-10 16:59:37 -0200

    urls: ussing BASE_DIR from settings

--- abyss/urls.py
+++ abyss/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.conf import settings
 
 import os
 
@@ -6,5 +7,5 @@ import os
 urlpatterns = [
     url(r'^', include('tsuru_dashboard.urls')),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': os.path.join(os.path.dirname(__file__), 'static')})
+        {'document_root': os.path.join(settings.BASE_DIR, 'static')})
 ]
