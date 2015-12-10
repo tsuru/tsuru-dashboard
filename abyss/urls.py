82f71539e71148706c04d942488f7354c13e48fd
Andrews Medina <andrewsmedina@gmail.com>
2015-12-10 16:49:03 -0200

    add url to serve static files

--- abyss/urls.py
+++ abyss/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url, include
+import os
+
+
 
 urlpatterns = [
     url(r'^', include('tsuru_dashboard.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(os.path.dirname(__file__), 'static')})
 ]
