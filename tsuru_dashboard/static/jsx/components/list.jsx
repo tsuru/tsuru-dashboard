b08b56c01a3c294616c7b44191239de5fbe0d8c6
André Carvalho <andre.carvalho@corp.globo.com>
2016-04-19 10:07:24 -0300

    apps: format js

--- tsuru_dashboard/static/jsx/components/list.jsx
+++ tsuru_dashboard/static/jsx/components/list.jsx
@@ -1,7 +1,7 @@
 var React = require('react'),
     fuzzy = require('fuzzy'),
     Loading = require('./loading.jsx'),
-	$ = require('jquery'),
+    $ = require('jquery'),
     PureRenderMixin = require('react-addons-pure-render-mixin');
 
 
@@ -87,14 +87,14 @@ var AppList = React.createClass({
   },
   appsByName: function(name) {
     if (this.state.loading) {
-        this.setState({term: name});    
+        this.setState({term: name});
         return;
     }
 
     if (this.state.cached.length == 0 ) {
       this.loadApps();
       return;
-    } 
+    }
     var options = {
       extract: function(el) { return el.name }
     };
