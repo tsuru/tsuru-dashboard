ca32edb6468518b767b59fedbe780233f5c94699
Andrews Medina <andrewsmedina@gmail.com>
2016-07-13 13:33:17 -0600

    events: move get events to a method

--- tsuru_dashboard/events/views.py
+++ tsuru_dashboard/events/views.py
@@ -9,16 +9,17 @@ from tsuru_dashboard.auth.views import LoginRequiredView
 class ListEvent(LoginRequiredView, TemplateView):
     template_name = "events/list.html"
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(ListEvent, self).get_context_data(*args, **kwargs)
+    def get_events(self):
         url = '{}/events'.format(settings.TSURU_HOST)
         response = requests.get(
             url, headers=self.authorization, params=self.request.GET)
 
         if response.status_code == 204:
-            events = []
-        else:
-            events = response.json()
+            return []
 
-        context['events'] = events
+        return response.json()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ListEvent, self).get_context_data(*args, **kwargs)
+        context['events'] = self.get_events()
         return context
