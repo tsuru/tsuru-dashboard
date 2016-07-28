364712af82b145bea4de4dacd8858f30c8c815a2
Andrews Medina <andrewsmedina@gmail.com>
2016-07-28 15:19:07 -0300

    js: Loading class should not be default

--- tsuru_dashboard/static/jsx/components/loading.js
+++ tsuru_dashboard/static/jsx/components/loading.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Loading extends Component {
+export class Loading extends Component {
   render() {
     return (
       <div className="loader">
