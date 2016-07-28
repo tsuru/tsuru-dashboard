83fb59aeca1d749649522a0b6e97a4bf08dd0449
Andrews Medina <andrewsmedina@gmail.com>
2016-07-28 11:08:08 -0300

    package.json: update deps

--- tsuru_dashboard/static/__tests__/loading-test.js
+++ tsuru_dashboard/static/__tests__/loading-test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Loading from "../jsx/components/loading.js";
+import { Loading } from "../jsx/components/loading.js";
 
 describe('Loading', () => {
   it('renders an element with "loader" class', () => {
