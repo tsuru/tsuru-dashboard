07a6a68bbaa61b50434d4ee6490bd069ea1d9df0
Paulo Sousa <paulo.sousa@corp.globo.com>
2016-07-28 13:33:35 -0300

    components: set custom components list using env var

--- tsuru_dashboard/components/views.py
+++ tsuru_dashboard/components/views.py
@@ -1,11 +1,12 @@
 from tsuru_dashboard.auth.views import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import TemplateView, View
 from django.http import JsonResponse
+from tsuru_dashboard import settings
 
 
 class ListComponentJson(LoginRequiredMixin, PermissionRequiredMixin, View):
     def get(self, *args, **kwargs):
-        component_list = {"components": ["registry", "big-sibling"]}
+        component_list = {"components": [x.strip() for x in settings.METRICS_COMPONENTS.split(",")]}
         return JsonResponse(component_list, safe=False)
 
 
