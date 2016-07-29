9a637089f722463185634939a4e744a63c2dc195
Andrews Medina <andrewsmedina@gmail.com>
2016-07-29 15:07:40 -0300

    js: change app list to use es6

--- tsuru_dashboard/static/__tests__/app-list.js
+++ tsuru_dashboard/static/__tests__/app-list.js
@@ -1,75 +1,73 @@
-// jest.dontMock('../jsx/components/list.jsx');
-// jest.dontMock('fuzzy');
-//
-// var React = require('react'),
-//     Enzyme = require('enzyme'),
-//     List = require('../jsx/components/list.jsx'),
-//     AppList = List.AppList,
-//     AppSearch = List.AppSearch,
-//     AppTable = List.AppTable,
-//     Loading = require('../jsx/components/loading.jsx'),
-//     $ = require('jquery');
-//
-// describe('AppList', function() {
-//   it('should has app-list as className', function() {
-//     const wrapper = Enzyme.shallow(
-//       <AppList url="http://localhost:80/apps/list.json" />
-//     );
-//     expect(wrapper.find(".app-list").length).toBe(1);
-//   });
-//
-//   it ('should be composed by AppSearch and AppTable', function() {
-//     const wrapper = Enzyme.shallow(
-//       <AppList url="http://localhost:80/apps/list.json" />
-//     );
-//     expect(wrapper.find(".app-list").children().length).toBe(2);
-//     expect(wrapper.find(AppSearch).length).toBe(1);
-//     expect(wrapper.find(AppTable).length).toBe(1);
-//   });
-//
-//   it ('should contain Loading if its loading', function() {
-//     const wrapper = Enzyme.shallow(
-//       <AppList url="http://localhost:80/apps/list.json" />
-//     );
-//     wrapper.setState({loading: true})
-//     expect(wrapper.find(Loading).length).toBe(1);
-//   });
-//
-//   it('should load apps on render', function() {
-//     const wrapper = Enzyme.mount(
-//       <AppList url="http://localhost:80/apps/list.json" />
-//     )
-//
-//     $.ajax.mock.calls[0][0].success({apps: [{name: "appname"}, {name: "otherapp"}]});
-//
-//     expect({apps: [{name: "appname"}, {name: "otherapp"}], cached: [{name: "appname"}, {name: "otherapp"}], loading: false, term: ''}).toEqual(wrapper.state());
-//
-//     var items = wrapper.find("td");
-//     expect(items.length).toBe(2);
-//   });
-//
-//   it('should filter list by app name', function() {
-//     const wrapper = Enzyme.mount(
-//       <AppList url="http://localhost:80/apps/list.json" />
-//     )
-//
-//     $.ajax.mock.calls[1][0].success({apps: [{name: "appname"}, {name: "other"}]});
-//
-//     wrapper.find("input").simulate('change', {target: {value: "oth"}});
-//     expect({apps: [{name: "other"}], cached: [{name: "appname"}, {name: "other"}], loading: false, term: ''}).toEqual(wrapper.state());
-//     expect(wrapper.find("td").length).toBe(1);
-//   });
-//
-//   it('should list all on empty search', function() {
-//     const wrapper = Enzyme.mount(
-//       <AppList url="http://localhost:80/apps/list.json" />
-//     )
-//
-//     $.ajax.mock.calls[2][0].success({apps: [{name: "appname"}, {name: "other"}]});
-//
-//     wrapper.find("input").simulate('change', {target: {value: ""}});
-//     expect({apps: [{name: "appname"}, {name: "other"}], cached: [{name: "appname"}, {name: "other"}], loading: false, term: ''}).toEqual(wrapper.state());
-// 	  expect(wrapper.find("td").length).toBe(2);
-//   });
-//
-// });
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { AppList, AppSearch, AppTable } from "../jsx/components/list";
+import { Loading } from "../jsx/components/loading";
+import $ from "jquery";
+
+describe('AppList', () => {
+  it('should has app-list as className', () => {
+    const wrapper = shallow(
+      <AppList url="http://localhost:80/apps/list.json" />
+    );
+    expect(wrapper.find(".app-list").length).toBe(1);
+  });
+
+  it ('should be composed by AppSearch and AppTable', () => {
+    const wrapper = shallow(
+      <AppList url="http://localhost:80/apps/list.json" />
+    );
+    expect(wrapper.find(".app-list").children().length).toBe(2);
+    expect(wrapper.find(AppSearch).length).toBe(1);
+    expect(wrapper.find(AppTable).length).toBe(1);
+  });
+
+  it ('should contain Loading if its loading', () => {
+    const wrapper = shallow(
+      <AppList url="http://localhost:80/apps/list.json" />
+    );
+    wrapper.setState({loading: true})
+    expect(wrapper.find(Loading).length).toBe(1);
+  });
+
+  it('should load apps on render', () => {
+    $.ajax = jest.genMockFunction();
+    const wrapper = mount(
+      <AppList url="http://localhost:80/apps/list.json" />
+    )
+
+    $.ajax.mock.calls[0][0].success({apps: [{name: "appname"}, {name: "otherapp"}]});
+
+    expect({apps: [{name: "appname"}, {name: "otherapp"}], cached: [{name: "appname"}, {name: "otherapp"}], loading: false, term: ''}).toEqual(wrapper.state());
+
+    var items = wrapper.find("td");
+    expect(items.length).toBe(2);
+  });
+
+  it('should filter list by app name', () => {
+    $.ajax = jest.fn((obj) => {
+      obj.success({apps: [{name: "appname"}, {name: "other"}]});
+    });
+    const wrapper = mount(
+      <AppList url="http://localhost:80/apps/list.json" />
+    )
+
+    wrapper.find("input").simulate('change', {target: {value: "oth"}});
+    expect({apps: [{name: "other"}], cached: [{name: "appname"}, {name: "other"}], loading: false, term: ''}).toEqual(wrapper.state());
+    expect(wrapper.find("td").length).toBe(1);
+  });
+
+  it('should list all on empty search', () => {
+    $.ajax = jest.fn((obj) => {
+      obj.success({apps: [{name: "appname"}, {name: "other"}]});
+    });
+
+    const wrapper = mount(
+      <AppList url="http://localhost:80/apps/list.json" />
+    )
+
+    wrapper.find("input").simulate('change', {target: {value: ""}});
+    expect({apps: [{name: "appname"}, {name: "other"}], cached: [{name: "appname"}, {name: "other"}], loading: false, term: ''}).toEqual(wrapper.state());
+	  expect(wrapper.find("td").length).toBe(2);
+  });
+
+});
---
+++ tsuru_dashboard/static/jsx/components/list.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import fuzzy from "fuzzy";
+import { Loading } from "./loading";
+import $ from "jquery";
+
+export class AppSearch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    var name = e.target.value.trim();
+    this.props.search(name);
+  }
+
+  render() {
+    return (
+      <div className="search">
+      <input type="text"
+      ref="name"
+      placeholder="search apps by name"
+      onChange={this.handleChange} />
+      <AppAdd />
+      <div className="clearfix"></div>
+      </div>
+    )
+  }
+}
+
+class AppAdd extends Component {
+  render() {
+    return (
+      <a title="new app" href="/apps/create/"><i className="icono-plus"></i></a>
+    )
+  }
+}
+
+export class App extends Component {
+  render() {
+    return (
+      <tr>
+      <td>
+      <a href={this.props.name} title="App Details">
+      {this.props.name}
+      </a>
+      </td>
+      </tr>
+    )
+  }
+}
+
+export class AppTable extends Component {
+  render() {
+    var appNodes = this.props.data.map(function(app) {
+      return (
+        <App key={app.name} name={app.name} url={app.url}/>
+      );
+    });
+    return (
+      <table className="table">
+      <tbody>{appNodes}</tbody>
+      </table>
+    )
+  }
+}
+
+export class AppList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cached: [],
+      apps: [],
+      loading: false,
+      term: ""
+    }
+
+    this.appsByName = this.appsByName.bind(this);
+  }
+
+  loadApps() {
+    this.setState({loading: true});
+    $.ajax({
+      type: 'GET',
+      url: this.props.url,
+      success: function(data) {
+        this.setState({cached: data.apps, apps: data.apps, loading: false});
+
+        if (this.state.term.length > 0) {
+          this.appsByName(this.state.term);
+          this.setState({term: ''});
+        }
+
+      }.bind(this)
+    });
+  }
+
+  appsByName(name) {
+    if (this.state.loading) {
+      this.setState({term: name});
+      return;
+    }
+
+    if (this.state.cached.length == 0 ) {
+      this.loadApps();
+      return;
+    }
+    var options = {
+      extract: function(el) { return el.name }
+    };
+    var results = fuzzy.filter(name, this.state.cached, options);
+    this.setState({apps: results.map(function(el) { return el.original; })});
+  }
+
+  componentDidMount() {
+    this.appsByName("");
+  }
+
+  render() {
+    return (
+      <div className="app-list">
+      <AppSearch search={this.appsByName} />
+      {this.state.loading ? <Loading /> : ''}
+      <AppTable data={this.state.apps} />
+      </div>
+    );
+  }
+}
