a2db8401f21077ab84662be94d095beeea87a22e
Andrews Medina <andrewsmedina@gmail.com>
2016-07-28 22:10:52 -0300

    js: change node-create to use es6

--- tsuru_dashboard/static/__tests__/node-create.js
+++ tsuru_dashboard/static/__tests__/node-create.js
@@ -1,60 +1,58 @@
-// jest.dontMock('../jsx/components/node-create.jsx');
-//
-// var React = require('react'),
-//     Enzyme = require('enzyme'),
-//     NodeCreate = require('../jsx/components/node-create.jsx');
-//
-// describe('NodeCreate', function() {
-//   it('should has node-create as className', function() {
-//     const nodeCreate = Enzyme.shallow(<NodeCreate />);
-//     expect(nodeCreate.find(".node-create").length).toBe(1);
-//   });
-//
-//   it('should initial state', function() {
-//     const nodeCreate = Enzyme.shallow(<NodeCreate />);
-//     var state = nodeCreate.state();
-//     expect(state.templates.length).toBe(0);
-//     expect(state.metadata.length).toBe(0);
-//     expect(state.register).toBeFalsy();
-//   });
-//
-//   it('change register state on click', function() {
-//     const nodeCreate = Enzyme.mount(<NodeCreate />);
-//     var register = nodeCreate.find(".register");
-//
-//     expect(nodeCreate.state().register).toBeFalsy();
-//     expect(nodeCreate.state().metadata.length).toEqual(0);
-//
-//     register.simulate('click');
-//     expect(nodeCreate.state().register).toBeTruthy();
-//     expect(nodeCreate.state().metadata).toEqual([{id: 1, key: "address", value: ""}]);
-//
-//     register.simulate('click');
-//     expect(nodeCreate.state().register).toBeFalsy();
-//     expect(nodeCreate.state().metadata).toEqual([]);
-//   });
-//
-//   it('don"t show template select on empty templates', function() {
-//     const nodeCreate = Enzyme.shallow(<NodeCreate />);
-//     var templates = nodeCreate.find(".template");
-//     expect(templates.length).toBe(0);
-//   });
-//
-//   it('metadata items', function() {
-//     const nodeCreate = Enzyme.mount(<NodeCreate />);
-//
-//     nodeCreate.get(0).addMetadata("key", "value");
-//     nodeCreate.get(0).addMetadata("anotherkey", "v");
-//
-//     var items = nodeCreate.find(".meta-item");
-//     expect(items.length).toEqual(2);
-//
-//     var inputs = items.children().find("input");
-//     expect(inputs.at(0).props().value).toBe("key");
-//     expect(inputs.at(1).props().value).toBe("value");
-//
-//     expect(inputs.at(2).props().value).toBe("anotherkey");
-//     expect(inputs.at(3).props().value).toBe("v");
-//   });
-//
-// });
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { NodeCreate } from "../jsx/components/node-create";
+
+describe('NodeCreate', () => {
+  it('should has node-create as className', () => {
+    const nodeCreate = shallow(<NodeCreate />);
+    expect(nodeCreate.find(".node-create").length).toBe(1);
+  });
+
+  it('should initial state', () => {
+    const nodeCreate = shallow(<NodeCreate />);
+    var state = nodeCreate.state();
+    expect(state.templates.length).toBe(0);
+    expect(state.metadata.length).toBe(0);
+    expect(state.register).toBeFalsy();
+  });
+
+  it('change register state on click', () => {
+    const nodeCreate = mount(<NodeCreate />);
+    var register = nodeCreate.find(".register");
+
+    expect(nodeCreate.state().register).toBeFalsy();
+    expect(nodeCreate.state().metadata.length).toEqual(0);
+
+    register.simulate('click');
+    expect(nodeCreate.state().register).toBeTruthy();
+    expect(nodeCreate.state().metadata).toEqual([{id: 1, key: "address", value: ""}]);
+
+    register.simulate('click');
+    expect(nodeCreate.state().register).toBeFalsy();
+    expect(nodeCreate.state().metadata).toEqual([]);
+  });
+
+  it('don"t show template select on empty templates', () => {
+    const nodeCreate = shallow(<NodeCreate />);
+    var templates = nodeCreate.find(".template");
+    expect(templates.length).toBe(0);
+  });
+
+  it('metadata items', () => {
+    const nodeCreate = mount(<NodeCreate />);
+
+    nodeCreate.get(0).addMetadata("key", "value");
+    nodeCreate.get(0).addMetadata("anotherkey", "v");
+
+    var items = nodeCreate.find(".meta-item");
+    expect(items.length).toEqual(2);
+
+    var inputs = items.children().find("input");
+    expect(inputs.at(0).props().value).toBe("key");
+    expect(inputs.at(1).props().value).toBe("value");
+
+    expect(inputs.at(2).props().value).toBe("anotherkey");
+    expect(inputs.at(3).props().value).toBe("v");
+  });
+
+});
---
+++ tsuru_dashboard/static/jsx/components/node-create.js
@@ -0,0 +1,350 @@
+import React, { Component } from "react";
+import $ from "jquery";
+
+class Options extends Component {
+  render() {
+    return (
+      <option
+        key={this.props.value}
+        value={this.props.value}>
+          {this.props.value}
+      </option>
+    )
+  }
+}
+
+class Template extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.props.selectTemplate(e.target.value);
+  }
+
+  render() {
+    var options = [];
+
+    this.props.templates.forEach((template) => {
+      options.push(<Option key={template.Name} value={template.Name} />);
+    });
+
+    return (
+      <div className="template">
+        <label>
+          Template:
+          <select onChange={this.onChange}>
+            <option>Select a template</option>
+            {options}
+          </select>
+        </label>
+      </div>
+    )
+  }
+}
+
+class Register extends Component {
+  render() {
+    return (
+      <div className="register" onClick={this.props.onClick}>
+        <label>
+          Register an already created node:
+          <input type="checkbox" onClick={this.onClick} />
+        </label>
+      </div>
+    )
+  }
+}
+
+class Meta extends Component {
+  render() {
+    var items = [];
+
+    this.props.metadata.forEach((metadata) => {
+      items.push(<MetaItem key={metadata.id} name={metadata.key} value={metadata.value} removeMetadata={this.props.removeMetadata} editMetadata={this.props.editMetadata} />);
+    });
+
+    return (
+      <div className="meta">
+        {items}
+      </div>
+    )
+  }
+}
+
+class MetaItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: this.props.name,
+      value: this.props.value
+    }
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.props.editMetadata(this.state.name, this.refs.name.value, this.refs.value.value);
+  }
+
+  removeMetadata() {
+    this.props.removeMetadata(this.refs.name.value);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({name: nextProps.name, value: nextProps.value});
+  }
+
+  render() {
+    return (
+      <div className="meta-item">
+        <label>
+          Key:
+          <input type="text" name="name" ref="name" value={this.state.name} onChange={this.onChange} />
+        </label>
+        <label>
+          Value:
+          <input type="text" name="value" ref="value" value={this.state.value} onChange={this.onChange} />
+        </label>
+        <button onClick={this.removeMetadata}>Remove item</button>
+      </div>
+    )
+  }
+}
+
+MetaItem.defaultProps = {
+  name: "",
+  value: ""
+}
+
+class Button extends Component {
+  render() {
+    return (
+      <button type={this.props.type}
+              disabled={this.props.disabled}
+              onClick={this.props.onClick}
+              className='btn'>
+        {this.props.text}
+      </button>
+    )
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+  onClick: function(){},
+  type: "button"
+}
+
+class CancelBtn extends Component {
+  render() {
+    return (
+      <button data-dismiss='modal'
+        disabled={this.props.disabled}
+              aria-hidden='true'
+              className='btn'
+              onClick={this.props.onClick}>
+        Cancel
+      </button>
+    )
+  }
+}
+
+CancelBtn.defaultProps = {
+  disabled: false
+}
+
+class Iaas extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      iaas: this.props.iaas
+    }
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.props.setIaas(e.target.value);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({iaas: nextProps.iaas});
+  }
+
+  render() {
+    return (
+      <div className="iaas">
+        <label>Iaas name: <input type="text" value={this.state.iaas} onChange={this.onChange} /></label>
+      </div>
+    );
+  }
+}
+
+export class NodeCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    function idMaker() { var initial = 0; return function() { initial++; return initial}}
+    this.state = {
+      templates: [],
+      iaas: "",
+      register: false,
+      metadata: [],
+      id: 0,
+      getId: idMaker(),
+      disabled: false
+    }
+
+    this.registerToggle = this.registerToggle.bind(this);
+  }
+
+  cancel() {
+    this.setState({metadata: [], register: false});
+  }
+
+  registerToggle() {
+    if (!this.state.register) {
+        this.addMetadata("address", "");
+    } else {
+        this.removeMetadata("address");
+    }
+    this.setState({register: !this.state.register});
+  }
+
+  getId() {
+    return this.state.getId();
+  }
+
+  metaIndexByKey(key) {
+    var index = -1;
+    var meta = this.state.metadata;
+    meta.forEach(function(metadata, i) {
+      if (metadata.key === key) {
+        index = i;
+      }
+    });
+    return index;
+  }
+
+  addMetadata(key, value) {
+    var metadata = this.state.metadata;
+    var m = {key: key, value: value};
+    var index = this.metaIndexByKey(key);
+    if (index === -1) {
+        m.id = this.getId();
+        metadata.push(m);
+        this.setState({metadata: metadata});
+    } else {
+        this.editMetadata(key, key, value);
+    }
+  }
+
+  removeMetadata(key) {
+    var index = this.metaIndexByKey(key);
+    if (index === -1 )
+      return;
+    var meta = this.state.metadata;
+    meta.splice(index, 1);
+    this.setState({metadata: meta});
+  }
+
+  editMetadata(key, newKey, newValue) {
+    var index = this.metaIndexByKey(key);
+    if (index === -1)
+        return;
+
+    var metadata = this.state.metadata;
+    var m = metadata[index];
+    m.key = newKey;
+    m.value = newValue;
+    metadata[index] = m;
+    this.setState({metadata: metadata});
+  }
+
+  add(e) {
+    e.preventDefault();
+    this.addMetadata("", "");
+  }
+
+  loadTemplates() {
+    $.ajax({
+      type: 'GET',
+      url: "/admin/templates.json",
+      success: function(data) {
+          this.setState({templates: data});
+      }.bind(this)
+    });
+  }
+
+  componentDidMount() {
+    this.loadTemplates();
+  }
+
+  selectTemplate(templateName) {
+    this.state.templates.forEach(function(template) {
+      if (template.Name === templateName) {
+        this.setIaas(template.IaaSName);
+        template.Data.forEach(function(metaData) {
+          this.addMetadata(metaData.Name, metaData.Value);
+        }.bind(this));
+      }
+    }.bind(this));
+  }
+
+  addNode() {
+    this.setState({disabled: true});
+    var url = "/admin/node/add/";
+    var data = [];
+    this.state.metadata.forEach(function(metadata) {
+      data.push("Metadata." + metadata.key + "=" + metadata.value);
+    });
+    if (this.state.iaas.length > 0) {
+      data.push("Metadata.iaas=" + this.state.iaas);
+    }
+    data.push("Metadata.pool=" + this.props.pool);
+    data.push("Register=" + this.state.register);
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: data.join("&"),
+      success: function() {
+        location.reload();
+      }.bind(this),
+      error: function(){
+        location.reload();
+      } .bind(this)
+    });
+  }
+
+  setIaas(iaas) {
+    this.setState({iaas: iaas});
+  }
+
+  render() {
+    return (
+      <div className="node-create modal-dialog">
+        <div className="modal-content">
+          <div className='modal-header'>
+            <h3 id='myModalLabel'>Create node</h3>
+          </div>
+          <div className='modal-body'>
+            {this.state.templates != null && this.state.templates.length > 0 ? <Template templates={this.state.templates} selectTemplate={this.selectTemplate} /> : ""}
+            <Register register={this.state.register} onClick={this.registerToggle} />
+            <Iaas iaas={this.state.iaas} />
+            <Meta metadata={this.state.metadata} removeMetadata={this.removeMetadata} editMetadata={this.editMetadata} />
+          </div>
+          <div className='modal-footer'>
+            <CancelBtn onClick={this.cancel} disabled={this.state.disabled} />
+            <Button text="Add metadata" onClick={this.add} disabled={this.state.disabled} />
+            <Button text="Create node" onClick={this.addNode} disabled={this.state.disabled} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
