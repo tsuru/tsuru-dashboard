c93519aa620394b40d25d754deac96ef62a88ee4
Andrews Medina <andrewsmedina@gmail.com>
2016-08-17 15:41:58 -0300

    using whitenoise to serve static files

--- abyss/wsgi.py
+++ abyss/wsgi.py
@@ -1,7 +1,10 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abyss.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
