17c7b58503e9950b3af926714dacd83d8ab1d17e
Andrews Medina <andrewsmedina@gmail.com>
2016-08-23 19:36:44 -0300

    event/list: fix bug on get querystring data

--- tsuru_dashboard/static/js/event-list.js
+++ tsuru_dashboard/static/js/event-list.js
@@ -21451,7 +21451,7 @@ function getParameterByName(name, url) {
   name = name.replace(/[\[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
       results = regex.exec(url);
-  if (!results) return null;
+  if (!results) return '';
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
