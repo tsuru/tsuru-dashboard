f2af09714cf9f624d51b216aa1a26f5a7453eff2
Andrews Medina <andrewsmedina@gmail.com>
2016-08-25 15:09:31 -0300

    js: move event cancel to a component

--- tsuru_dashboard/static/jsx/pages/event-info.js
+++ tsuru_dashboard/static/jsx/pages/event-info.js
@@ -1,21 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Modal from "backstage-modal";
+import EventCancel from "../components/event-cancel";
 
 $("#cancel-button").on('click', () => {
-ReactDOM.render(
-  <Modal title="cancel event" isOpen>
-    <div>
-      <p>This action CANNOT be undone.
-      This will cancel this event.</p>
-
-      <p>Type the reason here to continue:</p>
-      <p><input type="text" /></p>
-    </div>
-    <div>
-      <button className="button">cancel</button>
-    </div>
-  </Modal>,
-  document.getElementById('modal')
-);
+  ReactDOM.render(
+    <EventCancel />,
+    document.getElementById('modal')
+  );
 })
