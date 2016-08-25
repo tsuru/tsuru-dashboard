49c8632cbdbf79adb6411a6882a354f38017d819
Andrews Medina <andrewsmedina@gmail.com>
2016-08-25 16:04:32 -0300

    event/cancel: disable csrf check

--- tsuru_dashboard/events/urls.py
+++ tsuru_dashboard/events/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 import views
 
@@ -7,5 +8,5 @@ urlpatterns = [
     url(r'^$', views.ListEvent.as_view(), name='event-list'),
     url(r'^kinds/$', views.KindList.as_view(), name='kind-list'),
     url(r'^(?P<uuid>[\w-]+)/$', views.EventInfo.as_view(), name='event-info'),
-    url(r'^(?P<uuid>[\w-]+)/cancel/$', views.EventCancel.as_view(), name='event-cancel'),
+    url(r'^(?P<uuid>[\w-]+)/cancel/$', csrf_exempt(views.EventCancel.as_view()), name='event-cancel'),
 ]
