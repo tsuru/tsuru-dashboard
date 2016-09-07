6e8c58ba6aa2d987cb9e55a0ad6247c849c57fd9
Andrews Medina <andrewsmedina@gmail.com>
2016-09-07 00:18:43 -0300

    metrics/prometheus: add mem_max method

---
+++ tsuru_dashboard/metrics/backends/prometheus/__init__.py
@@ -0,0 +1,38 @@
+from tsuru_dashboard import settings
+
+import requests
+
+
+class MetricNotEnabled(Exception):
+    pass
+
+
+class Prometheus(object):
+    def __init__(self, url, query):
+        self.url = url
+        self.query = query
+
+    def mem_max(self, interval=None):
+        url = self.url
+        url += "/api/v1/query_range?"
+        url += 'query=avg(container_memory_usage_bytes{%s})/1024/1024&' % self.query
+        url += "start=1473209927.011&end=1473213527.011&step=14"
+        result = requests.get(url)
+        return result.json()
+
+
+class AppBackend(Prometheus):
+    def __init__(self, app, token, process_name=None, date_range=None):
+        headers = {'authorization': token}
+        url = "{}/apps/{}/metric/envs".format(settings.TSURU_HOST, app["name"])
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            if "METRICS_PROMETHEUS_HOST" in data:
+                return super(AppBackend, self).__init__(
+                    url=data["METRICS_PROMETHEUS_HOST"],
+                    query='name=~"%s.*"' % app["name"]
+                )
+
+        raise MetricNotEnabled
---
+++ tsuru_dashboard/metrics/backends/prometheus/test_prometheus.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from tsuru_dashboard.metrics.backends.prometheus import Prometheus
+from mock import patch
+
+
+class PrometheusTest(TestCase):
+    def setUp(self):
+        self.backend = Prometheus("http://url.com", query="key=value")
+
+    @patch("requests.get")
+    def test_mem_max(self, get_mock):
+        self.backend.mem_max()
+        expected = self.backend.url
+        expected += "/api/v1/query_range?"
+        expected += "query=avg(container_memory_usage_bytes{key=value})/1024/1024"
+        expected += "&start=1473209927.011&end=1473213527.011&step=14"
+        get_mock.assert_called_with(expected)
