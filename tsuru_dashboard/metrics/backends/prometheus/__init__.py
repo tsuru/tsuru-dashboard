099a99bfbfa0d2e119209585693a668ba7d1c1ce
Andrews Medina <andrewsmedina@gmail.com>
2016-09-11 18:08:17 -0300

    metrics/prometheus: extract get method

--- tsuru_dashboard/metrics/backends/prometheus/__init__.py
+++ tsuru_dashboard/metrics/backends/prometheus/__init__.py
@@ -6,13 +6,17 @@ class Prometheus(object):
         self.url = url
         self.query = query
 
-    def mem_max(self, interval=None):
+    def get_metrics(self, query):
         url = self.url
         url += "/api/v1/query_range?"
         url += 'query=avg(container_memory_usage_bytes{%s})/1024/1024&' % self.query
         url += "start=1473209927.011&end=1473213527.011&step=14"
         result = requests.get(url)
         return result.json()
+
+    def mem_max(self, interval=None):
+        query = "query=avg(container_memory_usage_bytes{%s})/1024/1024&" % self.query
+        return self.get_metrics(query)
 
 
 class AppBackend(Prometheus):
