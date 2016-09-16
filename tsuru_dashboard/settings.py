69d47a2849e9819b117fe27dfec996f7c7ee7861
Andrews Medina <andrewsmedina@gmail.com>
2016-09-16 16:27:22 -0300

    metrics: add settings for prometheus

--- tsuru_dashboard/settings.py
+++ tsuru_dashboard/settings.py
@@ -3,5 +3,6 @@ import os
 
 TSURU_HOST = os.environ.get("TSURU_HOST", "http://localhost:8080")
 ELASTICSEARCH_HOST = os.environ.get("ELASTICSEARCH_HOST")
+PROMETHEUS_HOST = os.environ.get("PROMETHEUS_HOST")
 ELASTICSEARCH_INDEX = os.environ.get("ELASTICSEARCH_INDEX", "measure-tsuru")
 METRICS_COMPONENTS = os.environ.get("METRICS_COMPONENTS", "registry, big-sibling")
