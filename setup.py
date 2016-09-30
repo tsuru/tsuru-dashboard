135791bf0fb7eb07b599ad34ba54e13d1eb75fea
Andrews Medina <andrewsmedina@gmail.com>
2016-09-30 16:19:45 -0300

    move required deps to setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,8 @@ setup(
         "Pygments>=2.0.2",
         "tsuruclient>=0.3.2",
         "grequests>=0.3.0",
+        "Pygments>=2.1.3",
+        "pymongo>=3.3.0",
+        "PyYAML>=3.11",
     ],
 )
