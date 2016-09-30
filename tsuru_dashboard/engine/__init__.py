6dbdb877a462e64179d17ddd3069c5e94fa5120f
Andrews Medina <andrewsmedina@gmail.com>
2016-09-30 18:26:29 -0300

    engine: add unregister method

--- tsuru_dashboard/engine/__init__.py
+++ tsuru_dashboard/engine/__init__.py
@@ -1,9 +1,20 @@
 apps = {}
 
 
+class AppNotFound(Exception):
+    pass
+
+
 def register(app):
     apps[app.name] = app
 
 
 def get(app_name):
+    if app_name not in apps:
+        raise AppNotFound
+
     return apps[app_name]
+
+
+def unregister(app_name):
+    del apps[app_name]
--- tsuru_dashboard/engine/tests.py
+++ tsuru_dashboard/engine/tests.py
@@ -1,13 +1,24 @@
 from django.test import TestCase
 
-from . import register, get
+from . import register, get, unregister, AppNotFound
 
 
 class RegisterTest(TestCase):
     def test_register(self):
         class MyApp(object):
             name = 'myapp'
+
         register(MyApp)
         my_app = get('myapp')
 
         self.assertEqual(my_app, MyApp)
+
+    def test_unregister(self):
+        class MyApp(object):
+            name = 'myapp'
+
+        register(MyApp)
+        unregister('myapp')
+
+        with self.assertRaises(AppNotFound):
+            get('myapp')
