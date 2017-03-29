8ec25cbe4ea097f935d27e6475259e5aa0c18c9b
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-03-29 14:32:13 -0300

    Fix tests

--- tsuru_dashboard/admin/tests/test_node_add_view.py
+++ tsuru_dashboard/admin/tests/test_node_add_view.py
@@ -1,69 +1,45 @@
-from mock import patch
+from mock import patch, Mock
 
 from django.test import TestCase
 from django.test.client import RequestFactory
 
-import httpretty
-
-from tsuru_dashboard import settings
 from tsuru_dashboard.admin.views import NodeAdd
 
 import json
 
 
+class FakeTsuruClient(object):
+    def __init__(self, error=""):
+        create = Mock()
+        attrs = {"iter_lines.return_value": [json.dumps({"Message": "", "Error": error})]}
+        create.configure_mock(**attrs)
+
+        self.nodes = Mock()
+        attrs = {"create.return_value": create}
+        self.nodes.configure_mock(**attrs)
+
 class NodeAddViewTest(TestCase):
-    def setUp(self):
-        httpretty.enable()
-
-    def tearDown(self):
-        httpretty.disable()
-        httpretty.reset()
-
+    @patch.object(NodeAdd, "client", FakeTsuruClient(error="something is wrong"))
     @patch("tsuru_dashboard.auth.views.token_is_valid")
     def test_view_register_false(self, token_is_valid):
         token_is_valid.return_value = True
 
-        factory = RequestFactory()
-        request = factory.post('/', data={"key": "value", "register": "false"})
+        request = RequestFactory().post('/', data={"key": "value", "register": "true"})
         request.session = {'tsuru_token': 'tokentest'}
-
-        url = "{}/docker/node".format(settings.TSURU_HOST)
-        httpretty.register_uri(
-            httpretty.POST,
-            url,
-            body=json.dumps({"Message": "", "Error": ""}),
-            status=200
-        )
-
         response = NodeAdd.as_view()(request)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("register", httpretty.last_request().parsed_body)
-        self.assertEqual("false", httpretty.last_request().parsed_body["register"][0])
-        self.assertIn("key", httpretty.last_request().parsed_body)
-        self.assertEqual("value", httpretty.last_request().parsed_body["key"][0])
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.content, "something is wrong")
 
+    @patch.object(NodeAdd, "client", FakeTsuruClient())
     @patch("tsuru_dashboard.auth.views.token_is_valid")
     def test_view_register_true(self, token_is_valid):
         token_is_valid.return_value = True
 
-        factory = RequestFactory()
-        request = factory.post('/', data={"key": "value", "register": "true"})
+        request = RequestFactory().post('/', data={"key": "value", "register": "true"})
         request.session = {'tsuru_token': 'tokentest'}
-
-        url = "{}/docker/node".format(settings.TSURU_HOST)
-
-        httpretty.register_uri(
-            httpretty.POST,
-            url,
-            body=json.dumps({"Message": "", "Error": ""}),
-            status=200
-        )
 
         response = NodeAdd.as_view()(request)
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn("register", httpretty.last_request().parsed_body)
-        self.assertEqual("true", httpretty.last_request().parsed_body["register"][0])
-        self.assertIn("key", httpretty.last_request().parsed_body)
-        self.assertEqual("value", httpretty.last_request().parsed_body["key"][0])
+        self.assertEqual(response.content, "Node was successfully created")
