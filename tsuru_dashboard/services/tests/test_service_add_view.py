f1b40ccf7232dcad3173146ea2681cc1c8102dc6
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-04-03 16:24:56 -0300

    Add tags in create service instance form

--- tsuru_dashboard/services/tests/test_service_add_view.py
+++ tsuru_dashboard/services/tests/test_service_add_view.py
@@ -25,6 +25,34 @@ class ServiceAddViewTest(TestCase):
             headers={'authorization': 'admin'},
             data={"name": "name", "owner": "team", "description": "desc", "plan": "plan"})
 
+    @mock.patch("requests.post")
+    @mock.patch("requests.get")
+    def test_post_with_tags(self, get, post):
+        get.return_value = mock.Mock(status_code=200)
+        post.return_value = mock.Mock(status_code=201)
+        data = {
+            "name": "name",
+            "team": "myteam",
+            "description": "mydescription",
+            "plan": "myplan",
+            "tags": " tag 1 , tag 2, tag 3  ,, , "
+        }
+        request = RequestFactory().post("/", data)
+        request.session = {"tsuru_token": "admin"}
+        response = ServiceAdd.as_view()(request, service_name="service")
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(reverse('service-list'), response.items()[1][1])
+        post.assert_called_with(
+            '{0}/services/service/instances'.format(settings.TSURU_HOST),
+            headers={'authorization': 'admin'},
+            data={
+                "name": "name",
+                "owner": "myteam",
+                "description": "mydescription",
+                "plan": "myplan",
+                "tag": ["tag 1", "tag 2", "tag 3"]
+            })
+
     @mock.patch("requests.get")
     def test_get(self, get):
         get.return_value = mock.Mock(status_code=200)
