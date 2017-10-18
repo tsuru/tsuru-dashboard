8031f807df493aec07f5ca70023528bc0dcc85d6
Artur Sousa <artur.sousa@corp.globo.com>
2017-10-18 02:12:51 -0200

    Create a TextInput react component for the text filters

---
+++ tsuru_dashboard/static/js/src/components/text-input.css.js
@@ -0,0 +1,11 @@
+export default {
+  textInput: {
+    border: '2px solid rgb(204, 204, 204)',
+    borderRadius: '4px',
+    fontFamily: 'Open Sans',
+    fontSize: '14px',
+    lineHeight: 1.1,
+    color: 'rgb(51, 51, 51)',
+    padding: '6px 0px 8px 16px'
+  }
+};
