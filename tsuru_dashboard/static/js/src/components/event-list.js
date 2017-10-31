82b3cb44280706c9c8075e14169d5d4d4fcb955c
Artur Sousa <artur.sousa@corp.globo.com>
2017-10-31 14:50:43 -0200

    Divide filters in 2 rows

--- tsuru_dashboard/static/js/src/components/event-list.js
+++ tsuru_dashboard/static/js/src/components/event-list.js
@@ -35,55 +35,59 @@ export class EventFilters extends Component {
     return (
       <div id='filter'>
         <form action='' method='GET' ref='form'>
-          <Dropdown
-            placeholder='choose an event kind'
-            options={this.state.options}
-            style={styles.eventFilter}
-            name='kindName'
-            value={this.props.kind}
-          />
-          <TextInput
-            placeholder='search by target'
-            name='target.value'
-            value={this.props.target}
-            style={styles.eventFilter}
-          />
-          <TextInput
-            placeholder='search by owner'
-            name='ownerName'
-            value={this.props.owner}
-            style={styles.eventFilter}
-          />
-          <DateTimeField
-            placeholder='search by since date'
-            name='since'
-            value={this.props.since}
-            style={styles.eventFilter}
-          />
-          <DateTimeField
-            placeholder='search by until date'
-            name='until'
-            value={this.props.until}
-            style={styles.eventFilter}
-          />
-          <SwitchButton
-            label='error only'
-            name='errorOnly'
-            style={styles.eventFilter}
-            checked={this.props.errorOnly}
-          />
-          <SwitchButton
-            label='running only'
-            name='running'
-            style={styles.eventFilter}
-            checked={this.props.running}
-          />
-          <SwitchButton
-            label='include removed'
-            name='includeRemoved'
-            style={styles.eventFilter}
-            checked={this.props.includeRemoved}
-          />
+          <div style={styles.eventFilterRow}>
+            <Dropdown
+              placeholder='choose an event kind'
+              options={this.state.options}
+              style={styles.eventFilter}
+              name='kindName'
+              value={this.props.kind}
+            />
+            <SwitchButton
+              label='error only'
+              name='errorOnly'
+              style={styles.eventFilter}
+              checked={this.props.errorOnly}
+            />
+            <SwitchButton
+              label='running only'
+              name='running'
+              style={styles.eventFilter}
+              checked={this.props.running}
+            />
+            <SwitchButton
+              label='include removed'
+              name='includeRemoved'
+              style={styles.eventFilter}
+              checked={this.props.includeRemoved}
+            />
+          </div>
+          <div style={styles.eventFilterRow}>
+            <TextInput
+              placeholder='target name'
+              name='target.value'
+              value={this.props.target}
+              style={styles.eventFilter}
+            />
+            <TextInput
+              placeholder='owner e-mail'
+              name='ownerName'
+              value={this.props.owner}
+              style={styles.eventFilter}
+            />
+            <DateTimeField
+              placeholder='from date'
+              name='since'
+              value={this.props.since}
+              style={styles.eventFilter}
+            />
+            <DateTimeField
+              placeholder='until date'
+              name='until'
+              value={this.props.until}
+              style={styles.eventFilter}
+            />
+          </div>
           <button ref='btn' type='submit'>filter</button>
         </form>
       </div>
