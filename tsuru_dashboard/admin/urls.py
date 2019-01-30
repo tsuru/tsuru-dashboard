873eeec18d2aeacd3f14c6add0a99bf941e5d3b5
Guilherme Rezende <guilhermebr@gmail.com>
2019-01-30 16:15:37 -0200

    admin: fix urls order

--- tsuru_dashboard/admin/urls.py
+++ tsuru_dashboard/admin/urls.py
@@ -4,14 +4,14 @@ import views
 
 urlpatterns = [
     url(r'^$', views.PoolList.as_view(), name='pool-list'),
-    url(r'^(?P<address>.+)/$', views.NodeInfo.as_view(), name='node-info'),
     url(r'^(?P<address>.+)/containers/$', views.NodeInfoJson.as_view(), name='node-info-json'),
-    url(r'^deploys/$', views.ListDeploy.as_view(), name='list-deploys'),
+    url(r'^(?P<address>.+)/$', views.NodeInfo.as_view(), name='node-info'),
     url(r'^deploys/(?P<deploy>[\s\w@\.-]+)/$', views.DeployInfo.as_view(), name='deploy-info'),
+    url(r'^deploys/$', views.ListDeploy.as_view(), name='list-deploys'),
     url(r'^healing/$', views.ListHealing.as_view(), name='list-healing'),
     url(r'^node/(?P<address>.+)/remove/$', views.NodeRemove.as_view(), name='node-remove'),
     url(r'^node/add/$', views.NodeAdd.as_view(), name='node-add'),
-    url(r'^pool/(?P<pool>[\w\\.-]+)/$', views.PoolInfo.as_view(), name='pool-info'),
     url(r'^pool/(?P<pool>[\w\\.-]+)/rebalance/$', views.PoolRebalance.as_view(), name='pool-rebalance'),
+    url(r'^pool/(?P<pool>[\w\\.-]+)/$', views.PoolInfo.as_view(), name='pool-info'),
     url(r'^templates.json$', views.TemplateListJson.as_view(), name='template-list-json'),
 ]
