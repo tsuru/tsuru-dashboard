5897994a99466c19fcd74f7fac062865e275f038
Claudio Netto <nettinhorama@gmail.com>
2019-03-08 11:01:58 -0300

    events: remove CSRF exempt statement from event cancel view

--- tsuru_dashboard/events/urls.py
+++ tsuru_dashboard/events/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.views.decorators.csrf import csrf_exempt
 
 import views
 
@@ -8,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.ListEvent.as_view(), name='event-list'),
     url(r'^kinds/$', views.KindList.as_view(), name='kind-list'),
     url(r'^(?P<uuid>[\w-]+)/$', views.EventInfo.as_view(), name='event-info'),
-    url(r'^(?P<uuid>[\w-]+)/cancel/$', csrf_exempt(views.EventCancel.as_view()), name='event-cancel'),
+    url(r'^(?P<uuid>[\w-]+)/cancel/$', views.EventCancel.as_view(), name='event-cancel'),
 ]
