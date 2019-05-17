d709792a57c3375f8e8b29db37f8284455dcd6b1
Cezar Sa Espinola <cezarsa@gmail.com>
2019-05-17 18:02:49 -0300

    metrics: read metrics from prometheus when running on kubernetes

--- tsuru_dashboard/metrics/backends/prometheus/__init__.py
+++ tsuru_dashboard/metrics/backends/prometheus/__init__.py
@@ -1,15 +1,22 @@
-import requests
-
-from datetime import datetime, timedelta
 from time import mktime
+from datetime import datetime, timedelta
+import logging
+
+import requests
 
 from tsuru_dashboard.metrics.backends import base
 
 
+logger = logging.getLogger(__name__)
+
+
+class NoDataException(Exception):
+    pass
+
+
 class Prometheus(object):
-    def __init__(self, url, query, date_range=None):
+    def __init__(self, url, date_range=None):
         self.url = url
-        self.query = query
         self.date_range = date_range
 
     @property
@@ -43,99 +50,167 @@ class Prometheus(object):
             if len(x) == 2:
                 return [x[0]*1000, x[1]]
             return x
-
         return list(map(toMs, results))
 
     def get_metrics(self, query, processor=None):
         url = self.url
-        url += "/api/v1/query_range?"
-        url += query
-        url += "start={}".format(mktime(self.start.timetuple()))
-        url += "&end={}".format(mktime(self.end.timetuple()))
-        url += "&step={}".format(self.resolution)
-        result = requests.get(url)
+        url += "/api/v1/query_range"
+        params = {
+            'query': query,
+            'start': mktime(self.start.timetuple()),
+            'end': mktime(self.end.timetuple()),
+            'step': self.resolution,
+        }
+        result = requests.get(url, params=params)
+        response_data = result.json()
+        logger.debug("Prometheus request: {}\nPrometheus response {}: {} - {}".format(
+                     result.url, result.status_code, result.headers, response_data))
 
         if processor is None:
-            result = result.json()['data']['result'][0]['values']
+            result_data = response_data.get('data', {}).get('result', [])
+            if len(result_data) == 0:
+                raise NoDataException()
+            response_data = result_data[0].get('values', [])
             processor = self.default_processor
 
-        return processor(result)
+        return processor(response_data)
+
+
+class AppBackend(Prometheus):
+
+    def __init__(self, app, url, process_name=None, date_range=None):
+        query = 'container_label_app_name="%s"' % app["name"]
+        if process_name is not None:
+            query += ',container_label_app_process="%s"' % process_name
+        self.app_name = app["name"]
+        self.process_name = process_name
+        self.app_metric_templates = [
+            '{container_metric}{{container_name=~"{app_name}-{process_name}.*",{extra_query}}}',
+            '{container_metric}{{container_name="POD",pod_name=~"{app_name}-{process_name}.*",{extra_query}}}',
+            '{container_metric}{{container_label_app_name="{app_name}",container_label_app_process{process_name_op}"{process_name}",{extra_query}}}',
+        ]
+        return super(AppBackend, self).__init__(
+            url=url,
+            date_range=date_range,
+        )
 
     def mem_max(self, interval=None):
         data = {"min": 0, "max": 1024}
-        query = "query=avg(container_memory_usage_bytes{%s})/1024/1024&" % self.query
+        query = self.make_query(
+            "container_memory_usage_bytes",
+            "avg({})/1024/1024",
+        )
         data["avg"] = self.get_metrics(query)
 
-        query = "query=max(container_memory_usage_bytes{%s})/1024/1024&" % self.query
+        query = self.make_query(
+            "container_memory_usage_bytes",
+            "max({})/1024/1024",
+        )
         data["max"] = self.get_metrics(query)
 
-        query = "query=min(container_memory_usage_bytes{%s})/1024/1024&" % self.query
+        query = self.make_query(
+            "container_memory_usage_bytes",
+            "min({})/1024/1024",
+        )
         data["min"] = self.get_metrics(query)
         return {"data": data}
 
     def cpu_max(self, interval=None):
         data = {"min": 0, "max": 100}
-        query = "query=avg(sum(rate(container_cpu_usage_seconds_total{%s}[2m])) by (name)) * 100&" % self.query
+        query = self.make_query(
+            "container_cpu_usage_seconds_total",
+            "avg(sum(rate({}[2m])) by (name)) * 100",
+        )
         data["avg"] = self.get_metrics(query)
 
-        query = "query=max(sum(rate(container_cpu_usage_seconds_total{%s}[2m])) by (name)) * 100&" % self.query
+        query = self.make_query(
+            "container_cpu_usage_seconds_total",
+            "max(sum(rate({}[2m])) by (name)) * 100",
+        )
         data["max"] = self.get_metrics(query)
 
-        query = "query=min(sum(rate(container_cpu_usage_seconds_total{%s}[2m])) by (name)) * 100&" % self.query
+        query = self.make_query(
+            "container_cpu_usage_seconds_total",
+            "min(sum(rate({}[2m])) by (name)) * 100",
+        )
         data["min"] = self.get_metrics(query)
         return {"data": data}
 
     def units(self, interval=None):
         data = {"min": 0, "max": 100}
-        query = "query=max(count(rate(container_memory_usage_bytes{%s}[2m])) by (slave))&" % self.query
+        query = self.make_query(
+            "container_memory_usage_bytes",
+            "max(count(rate({}[2m])) by (slave))",
+        )
         data["units"] = self.get_metrics(query)
         return {"data": data}
 
     def swap(self, interval=None):
         data = {"min": 0, "max": 1024}
-        query = "query=avg(container_memory_swap{%s})/1024/1024&" % self.query
+        query = self.make_query(
+            "container_memory_swap",
+            "avg({})/1024/1024",
+        )
         data["avg"] = self.get_metrics(query)
 
-        query = "query=max(container_memory_swap{%s})/1024/1024&" % self.query
+        query = self.make_query(
+            "container_memory_swap",
+            "max({})/1024/1024",
+        )
         data["max"] = self.get_metrics(query)
 
-        query = "query=min(container_memory_swap{%s})/1024/1024&" % self.query
+        query = self.make_query(
+            "container_memory_swap",
+            "min({})/1024/1024",
+        )
         data["min"] = self.get_metrics(query)
         return {"data": data}
 
     def netrx(self, interval=None):
         data = {"min": 0, "max": 100}
-        query = "query=rate(container_network_receive_bytes_total{%s}[2m])/1024&" % self.query
+        query = self.make_query(
+            "container_network_receive_bytes_total",
+            "sum(rate({}[2m]))/1024",
+        )
         data["netrx"] = self.get_metrics(query)
         return {"data": data}
 
     def nettx(self, interval=None):
         data = {"min": 0, "max": 100}
-        query = "query=rate(container_network_transmit_bytes_total{%s}[2m])/1024&" % self.query
+        query = self.make_query(
+            "container_network_transmit_bytes_total",
+            "sum(rate({}[2m]))/1024",
+        )
         data["nettx"] = self.get_metrics(query)
         return {"data": data}
 
     def connections(self, interval=None):
         data = {"min": 0, "max": 100}
-        query = 'query=sum(container_connections{%s,state="ESTABLISHED",protocol="tcp"}) by (destination)&' % self.query
+        query = self.make_query(
+            "container_connections",
+            "sum({}) by (destination)",
+            query='state="ESTABLISHED",protocol="tcp"',
+        )
         data['data'] = self.get_metrics(query, processor=self.connections_processor)
         return data
 
     def connections_processor(self, result):
-        results = result.json()['data']['result']
+        results = result['data']['result']
         data = {}
         for r in results:
             data[base.set_destination_hostname(r['metric']['destination'])] = self.default_processor(r['values'])
         return data
 
-
-class AppBackend(Prometheus):
-    def __init__(self, app, url, process_name=None, date_range=None):
-        query = 'container_label_app_name="%s"' % app["name"]
-        if process_name is not None:
-            query += ',container_label_app_process="%s"' % process_name
-        return super(AppBackend, self).__init__(
-            url=url,
-            query=query,
-            date_range=date_range,
-        )
+    def make_query(self, metric, agg, query="", container_name_label="container_name"):
+        args = {
+            "container_metric": metric,
+            "app_name": self.app_name,
+            "process_name": self.process_name or "",
+            "container_name_label": container_name_label,
+            "extra_query": query,
+        }
+        args["process_name_op"] = "=" if self.process_name else "!="
+        result = []
+        for tpl in self.app_metric_templates:
+            result.append(agg.format(tpl.format(**args)))
+        return ' or '.join(result)
--- tsuru_dashboard/metrics/backends/prometheus/test_prometheus.py
+++ tsuru_dashboard/metrics/backends/prometheus/test_prometheus.py
@@ -1,37 +1,50 @@
 from django.test import TestCase
-from tsuru_dashboard.metrics.backends.prometheus import Prometheus
+from tsuru_dashboard.metrics.backends.prometheus import Prometheus, AppBackend
 from freezegun import freeze_time
 
-from mock import patch
+from mock import patch, Mock
 from datetime import datetime, timedelta
 from time import mktime
 
 
-class PrometheusTest(TestCase):
+class AppBackendTest(TestCase):
     def setUp(self):
-        self.backend = Prometheus("http://url.com", query="key=value")
+        self.backend = AppBackend({"name": "myapp"}, "http://url.com", process_name="myprocess")
 
     @freeze_time("2012-04-01 16:32:15", tz_offset=0)
     @patch("requests.get")
     def test_mem_max(self, get_mock):
+        response_mock = Mock(status_code=200)
+        response_mock.json.return_value = {
+            "data": {
+                "result": [{
+                    "values": [],
+                }],
+            },
+        }
+        get_mock.return_value = response_mock
+
         self.backend.mem_max()
         start = datetime.now() - timedelta(hours=1)
         end = datetime.now()
-        expected = self.backend.url
-        expected += "/api/v1/query_range?"
-        expected += "query=min(container_memory_usage_bytes{key=value})/1024/1024"
-        expected += "&start={}&end={}&step={}".format(
-            mktime(start.timetuple()),
-            mktime(end.timetuple()),
-            self.backend.resolution,
-        )
-        get_mock.assert_called_with(expected)
+        expected_url = self.backend.url + "/api/v1/query_range"
+        expected_params = {
+            'query': 'min(container_memory_usage_bytes{container_name=~"myapp-myprocess.*",})/1024/1024 or min(container_memory_usage_bytes{container_name="POD",pod_name=~"myapp-myprocess.*",})/1024/1024 or min(container_memory_usage_bytes{container_label_app_name="myapp",container_label_app_process="myprocess",})/1024/1024',
+            'start': mktime(start.timetuple()),
+            'end': mktime(end.timetuple()),
+            'step': self.backend.resolution,
+        }
+        get_mock.assert_called_with(expected_url, params=expected_params)
+
+
+class PrometheusTest(TestCase):
+    def setUp(self):
+        self.backend = Prometheus("http://url.com")
 
     @freeze_time("2012-04-01 16:32:15", tz_offset=0)
     def test_default_date_range(self):
         backend = Prometheus(
             "http://url.com",
-            query="key=value",
         )
         self.assertEqual(backend.start, datetime.now() - timedelta(hours=1))
         self.assertEqual(backend.end, datetime.now())
@@ -40,7 +53,6 @@ class PrometheusTest(TestCase):
     def test_hour_date_range(self):
         backend = Prometheus(
             "http://url.com",
-            query="key=value",
             date_range="1h",
         )
         self.assertEqual(backend.start, datetime.now() - timedelta(hours=1))
@@ -48,7 +60,6 @@ class PrometheusTest(TestCase):
 
         backend = Prometheus(
             "http://url.com",
-            query="key=value",
             date_range="3h",
         )
         self.assertEqual(backend.start, datetime.now() - timedelta(hours=3))
@@ -58,7 +69,6 @@ class PrometheusTest(TestCase):
     def test_date_range_by_days(self):
         backend = Prometheus(
             "http://url.com",
-            query="key=value",
             date_range="1d",
         )
         self.assertEqual(backend.start, datetime.now() - timedelta(days=1))
@@ -66,7 +76,6 @@ class PrometheusTest(TestCase):
 
         backend = Prometheus(
             "http://url.com",
-            query="key=value",
             date_range="3d",
         )
         self.assertEqual(backend.start, datetime.now() - timedelta(days=3))
@@ -76,7 +85,6 @@ class PrometheusTest(TestCase):
     def test_date_range_by_week(self):
         backend = Prometheus(
             "http://url.com",
-            query="key=value",
             date_range="1w",
         )
         self.assertEqual(backend.start, datetime.now() - timedelta(days=7))
@@ -84,7 +92,6 @@ class PrometheusTest(TestCase):
 
         backend = Prometheus(
             "http://url.com",
-            query="key=value",
             date_range="2w",
         )
         self.assertEqual(backend.start, datetime.now() - timedelta(days=14))
@@ -94,7 +101,6 @@ class PrometheusTest(TestCase):
     def test_resolution(self):
         backend = Prometheus(
             "http://url.com",
-            query="key=value",
             date_range="1w",
         )
         resolution = (backend.end - backend.start).total_seconds() / 250
