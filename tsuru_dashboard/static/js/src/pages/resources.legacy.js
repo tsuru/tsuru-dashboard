8ef9a0c54e6c3fa93bc4359c03670a663d048e3a
Wilson Júnior <wilsonpjunior@gmail.com>
2020-06-15 18:31:38 -0300

    Add support to fallback on legacy metrics when grafana is not available

--- tsuru_dashboard/static/js/src/pages/resources.legacy.js
+++ tsuru_dashboard/static/js/src/pages/resources.legacy.js
@@ -3,7 +3,31 @@ import ReactDOM from "react-dom";
 import { Resources } from "../components/resources.legacy";
 
 var url = "/apps/" + window.location.pathname.split("/")[2] + ".json";
-ReactDOM.render(
-  <Resources url={url} />,
-  document.getElementById('resources')
-);
+var settings = JSON.parse(document.getElementById("settings").textContent);
+var resources = document.getElementById("resources");
+
+if (settings.GRAFANA_URL) {
+  const req = new Request(settings.GRAFANA_URL, { mode: "no-cors" });
+  fetch(req)
+    .then(function () {
+      ReactDOM.render(
+        <iframe className="grafana_frame" src={settings.GRAFANA_URL} />,
+        resources
+      );
+    })
+    .catch(function (err) {
+      console.info(
+        "Failed to load grafana metrics, fallback to legacy backend",
+        err
+      );
+      ReactDOM.render(
+        <Resources url={url} settings={settings} />,
+        resources
+      );
+    });
+} else {
+  ReactDOM.render(
+    <Resources url={url} settings={settings} />,
+    resources
+  );
+}
