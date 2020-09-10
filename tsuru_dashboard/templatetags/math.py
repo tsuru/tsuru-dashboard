3e4010afb10d2cca7fc3bcbec63cc2c2575e31f4
Wilson Júnior <wilsonpjunior@gmail.com>
2020-09-10 16:52:59 -0300

    Add CPU information on settings page

---
+++ tsuru_dashboard/templatetags/math.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def divide(value, arg):
+    try:
+        return "%g" % (float(value) / int(arg))
+    except (ValueError, ZeroDivisionError):
+        return None
